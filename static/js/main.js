78f966e24e0f38bc09a6a602d00939cc58db2b9e
Steffen Heger <steffen.heger@gmail.com>
2024-02-15 13:03:00 +0100

    add static/js, add basic user registration form

---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+async function test_GET() {
+    const response = await fetch("http://localhost:3030/static/test.json");
+    const test_obj = await response.json();
+    console.log(test_obj);
+}
+
+// Example POST method implementation:
+async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "same-origin", // no-cors, *cors, same-origin
+    //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //   credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    //   redirect: "follow", // manual, *follow, error
+    //   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+  
+async function test_POST() {
+  let username = document.getElementById(':r87:-form-item').value;
+  console.log(username);
+    postData("http://localhost:3030/test-post", { id: 0, field: "42" }).then((data) => {
+        console.log(data);
+    });
+}
