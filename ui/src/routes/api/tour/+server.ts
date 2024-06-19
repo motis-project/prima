2ee565fef334532ab09a715a81c2793d2b84a9c4
nilspenzel <116674699+nilspenzel@users.noreply.github.com>
2024-06-19 15:24:40 +0200

    Update tour in db (#18)
    * update column vehicle in table tour in database
    
    * format
    
    * check for tour overlap in ui
    
    * format

---
+++ ui/src/routes/api/tour/+server.ts
@@ -0,0 +1,36 @@
+import { json } from '@sveltejs/kit';
+import { db } from '$lib/database';
+import { sql } from 'kysely';
+
+export const POST = async ({ request }) => {
+	const { tour_id, vehicle_id } = await request.json();
+	await db.transaction().execute(async (trx) => {
+		sql`LOCK TABLE tour IN ACCESS EXCLUSIVE MODE;`.execute(trx);
+		const moved_tours = await trx
+			.selectFrom('tour')
+			.where('id', '=', tour_id)
+			.selectAll()
+			.execute();
+		if (moved_tours.length > 0) {
+			const colliding_tours = await trx
+				.selectFrom('tour')
+				.where('vehicle', '=', vehicle_id)
+				.where(({ eb }) =>
+					eb.and([
+						eb('tour.departure', '<', moved_tours[0].arrival),
+						eb('tour.arrival', '>', moved_tours[0].departure)
+					])
+				)
+				.selectAll()
+				.execute();
+			if (colliding_tours.length == 0) {
+				await trx
+					.updateTable('tour')
+					.set({ vehicle: vehicle_id })
+					.where('id', '=', tour_id)
+					.executeTakeFirst();
+			}
+		}
+	});
+	return json({});
+};
