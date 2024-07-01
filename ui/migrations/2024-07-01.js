70ac19f155c2ca6078578801a3d4d0d6c2602ead
Steffen Heger <steffen.heger@gmail.com>
2024-07-01 16:02:21 +0200

    create relations zone and company in migrations

--- ui/migrations/2024-07-01.js
+++ ui/migrations/2024-07-01.js
@@ -1,23 +1,23 @@
 import { sql } from 'kysely';
 export async function up(db) {
     await db.schema
-        .createTable('person')
+        .createTable('zone')
         .addColumn('id', 'serial', (col) => col.primaryKey())
-        .addColumn('first_name', 'varchar', (col) => col.notNull())
-        .addColumn('last_name', 'varchar')
-        .addColumn('gender', 'varchar(50)', (col) => col.notNull())
-        .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql `now()`).notNull())
         .execute();
+
     await db.schema
-        .createTable('pet')
+        .createTable('company')
         .addColumn('id', 'serial', (col) => col.primaryKey())
-        .addColumn('name', 'varchar', (col) => col.notNull().unique())
-        .addColumn('owner_id', 'integer', (col) => col.references('person.id').onDelete('cascade').notNull())
-        .addColumn('species', 'varchar', (col) => col.notNull())
+        .addColumn('latitude', 'real', (col) => col.notNull())
+        .addColumn('longitude', 'real', (col) => col.notNull())
+        .addColumn('display_name', 'varchar', (col) => col.notNull())
+        .addColumn('email', 'varchar', (col) => col.notNull().unique())
+        .addColumn('zone', 'integer', (col) =>
+            col.references('zone.id').onDelete('cascade').notNull(),
+        )
         .execute();
-    await db.schema.createIndex('pet_owner_id_index').on('pet').column('owner_id').execute();
 }
 export async function down(db) {
-    await db.schema.dropTable('pet').execute();
-    await db.schema.dropTable('person').execute();
+    await db.schema.dropTable('zone').execute();
+    await db.schema.dropTable('company').execute();
 }
