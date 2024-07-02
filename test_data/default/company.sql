5a2da46f7cd883a99a3895758344738432fd93ac
Steffen Heger <71780550+steffenheger@users.noreply.github.com>
2024-07-02 16:45:18 +0200

    Test data (#40)
    * Add drop table statements for all tables. Closes #32
    
    * Add test data for default test scenario. Closes #38
    
    * Update test data for default test scenario. Closes #38

---
+++ test_data/default/company.sql
@@ -0,0 +1,4 @@
+INSERT INTO "company" ("id", "latitude", "longitude", "display_name", "email", "zone") VALUES
+(1,	14.097294853223417, 51.27571523301026,	'Taxi-Unternehmen Bautzen-1',	'a@b',	2),
+(2,	14.43063772516797, 51.18775568198066,	'Taxi-Unternehmen Bautzen-2',	'd@e',	1),
+(3,	14.839784424630352, 51.29273054080147,	'Taxi-Unternehmen Görlitz-1',	'f@g',	3)
