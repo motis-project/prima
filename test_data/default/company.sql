b0853ab0c12fe4b40df4ee97d9e9ba2fee430ffd
Steffen Heger <steffen.heger@gmail.com>
2024-07-02 11:56:36 +0200

    Add test data for default test scenario. Closes #38

---
+++ test_data/default/company.sql
@@ -0,0 +1,9 @@
+INSERT INTO "company" ("id", "latitude", "longitude", "display_name", "email", "zone") VALUES
+(1,	51.17994,	14.000301,	'Taxi-Unternehmen Bautzen-1',	'a@b',	2),
+(2,	14.034681,	51.316338,	'Taxi-Unternehmen Bautzen-2',	'b@c',	2),
+(3,	14.179674,	51.46705,	'Taxi-Unternehmen Bautzen-3',	'c@d',	2),
+(4,	14.244972,	51.27251,	'Taxi-Unternehmen Bautzen-4',	'd@e',	1),
+(5,	14.381821,	51.169106,	'Taxi-Unternehmen Bautzen-5',	'e@f',	1),
+(6,	14.70897,	51.43354,	'Taxi-Unternehmen Görlitz-1',	'f@g',	3),
+(7,	14.879525,	51.221657,	'Taxi-Unternehmen Görlitz-2',	'g@h',	3),
+(8,	14.7537365,	51.0419,	'Taxi-Unternehmen Görlitz-3',	'h@i',	3);
--- test_data/load.sh
+++ test_data/load.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 BASEDIR=$(dirname $0)
-PGPASSWORD=pw psql postgresql://localhost:6500/prima --user postgres < $BASEDIR/test_data.sql
+SCENARIO=$1
+
+if [ $# -eq 0 ]
+  then
+    echo "No argument supplied\nTry: sh load.sh <path to sql dumps>"
+    exit 1
+fi
+
+
+PGPASSWORD=pw psql postgresql://localhost:6500/prima --user postgres < $BASEDIR/$SCENARIO/zone.sql
+PGPASSWORD=pw psql postgresql://localhost:6500/prima --user postgres < $BASEDIR/$SCENARIO/company.sql
+PGPASSWORD=pw psql postgresql://localhost:6500/prima --user postgres < $BASEDIR/$SCENARIO/vehicle.sql
+PGPASSWORD=pw psql postgresql://localhost:6500/prima --user postgres < $BASEDIR/$SCENARIO/availability.sql
+PGPASSWORD=pw psql postgresql://localhost:6500/prima --user postgres < $BASEDIR/$SCENARIO/tour.sql
