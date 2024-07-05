4ea4833b387bbc81457dee88491adaa85a7c679d
nilspenzel <116674699+nilspenzel@users.noreply.github.com>
2024-07-05 16:03:40 +0200

    Remove id column in test_data/default files (#49)
    auto-generate ids instead
    	closes #44

--- test_data/default/vehicle.sql
+++ test_data/default/vehicle.sql
@@ -1,7 +1,7 @@
-INSERT INTO "vehicle" ("id", "license_plate", "company", "seats", "wheelchair_capacity", "storage_space") VALUES
-(1,	'BZ-TU-11',	1,	3,	0,	0),
-(2,	'BZ-TU-12',	1,	3,	0,	0),
-(3,	'BZ-TU-21',	2,	3,	0,	0),
-(4,	'BZ-TU-22',	2,	3,	0,	0),
-(5,	'GR-TU-11',	3,	3,	0,	0),
-(6, 'GR-TU-12', 3,	3,	0,	0)
+INSERT INTO "vehicle" ("license_plate", "company", "seats", "wheelchair_capacity", "storage_space") VALUES
+('BZ-TU-11',	1,	3,	0,	0),
+('BZ-TU-12',	1,	3,	0,	0),
+('BZ-TU-21',	2,	3,	0,	0),
+('BZ-TU-22',	2,	3,	0,	0),
+('GR-TU-11',	3,	3,	0,	0),
+('GR-TU-12',    3,	3,	0,	0)
