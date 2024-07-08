f41ab2ab4229d21652dcf92ac8dc92e4a63a8c2f
Steffen Heger <steffen.heger@gmail.com>
2024-07-08 15:50:52 +0200

    remove IDs from sql insert statements

--- test_data/default/address.sql
+++ test_data/default/address.sql
@@ -1,3 +1,3 @@
-INSERT INTO "address" ("id", "street", "house_number", "postal_code", "city") VALUES
-(1,	'Start',	'1',	'02826',	'Görlitz'),
-(2,	'Ziel',	'1',	'02826',	'Görlitz');
+INSERT INTO "address" ("street", "house_number", "postal_code", "city") VALUES
+('Start',	'1',	'02826',	'Görlitz'),
+('Ziel',	'1',	'02826',	'Görlitz');
--- test_data/default/event.sql
+++ test_data/default/event.sql
@@ -1,3 +1,3 @@
-INSERT INTO "event" ("id", "is_pickup", "latitude", "longitude", "scheduled_time", "communicated_time", "address", "tour") VALUES
-(1,	't',	0,	0,	'2024-06-09 09:10:00',	'2024-06-09 09:10:00',	1,	2),
-(2,	'f',	0,	0,	'2024-06-09 10:00:00',	'2024-06-09 10:00:00',	2,	2);
+INSERT INTO "event" ("is_pickup", "latitude", "longitude", "scheduled_time", "communicated_time", "address", "tour") VALUES
+('t',	0,	0,	'2024-06-09 09:10:00',	'2024-06-09 09:10:00',	1,	2),
+('f',	0,	0,	'2024-06-09 10:00:00',	'2024-06-09 10:00:00',	2,	2);
