ef7f61dff02b3380c70ab0e7336381f881f846a8
nilspenzel <penzel.nils@web.de>
2024-07-09 17:53:21 +0200

    Add Navigation Bar to all Routes except Signup

---
+++ src/routes/(user)/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../../app.css';
+	import Nav from '$lib/components/ui/nav.svelte';
+</script>
+
+<div class="flex min-h-screen">
+	<div class="container min-w-[1440px] relative pb-10 mt-[5%]">
+		<section>
+			<Nav />
+			<div class="overflow-hidden rounded-[0.5rem] border bg-background shadow-xl">
+				<slot />
+			</div>
+		</section>
+	</div>
+</div>
