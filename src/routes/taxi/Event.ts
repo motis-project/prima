7effe3d5c5684cf6c9c94007e692b6536e6e4564
Steffen Heger <steffen.heger@gmail.com>
2024-07-09 11:14:57 +0200

    fix formatting, wip

--- src/routes/taxi/Event.ts
+++ src/routes/taxi/Event.ts
@@ -1,10 +1,10 @@
 export class Event {
 	id!: number;
-    is_pickup!: boolean;
-    latitude!: number;
-    longitude!: number;
-    scheduled_time!: Date;
-    communicated_time!: Date;
+	is_pickup!: boolean;
+	latitude!: number;
+	longitude!: number;
+	scheduled_time!: Date;
+	communicated_time!: Date;
 	street!: string;
 	house_number!: string;
 	postal_code!: string;
