b9e5991a07fb1561e81131c3d636e8ddeeadcdde
Steffen Heger <steffen.heger@gmail.com>
2024-07-12 13:35:41 +0200

    update default test data

--- test_data/default/address.sql
+++ test_data/default/address.sql
@@ -1,3 +1,7 @@
 INSERT INTO "address" ("street", "house_number", "postal_code", "city") VALUES
-('Start',	'1',	'02826',	'Görlitz'),
-('Ziel',	'1',	'02826',	'Görlitz');
+('Am Schloss',	'107',	'02829',	'Markersdorf'),
+('Ziel',	'1',	'02826',	'Görlitz'),
+('Gersdorfer Straße',	'5',	'02894',	'Reichenbach'),
+('Otto-Müller-Straße ',	'3',	'02826',	'Görlitz'),
+('Kirchweg',	'12',	'02829',	'Markersdorf'),
+('Bahnhofstraße',	'76',	'02826',	'Görlitz');
