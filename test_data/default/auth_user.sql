028aad6e8094b85c742ff9b77b223384f96e9ae5
nilspenzel <penzel.nils@web.de>
2024-07-18 14:50:46 +0200

    Adjust test data
    	add columns is_entrpreneur and is_admin to auth_user

--- test_data/default/auth_user.sql
+++ test_data/default/auth_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO "auth_user" ("id", "email", "password_hash", "first_name", "last_name", "phone") VALUES
-('ggf0qg7u0umvarr',	'foo@bar.com',	'$argon2id$v=19$m=19456,t=2,p=1$2wXe3uySLfiShxmVsDx7ig$Ycz29Te/viPxg/LoyFSywrsN+kUinyTtSrUehGu7qGg', 'John', 'Doe', '0815-12345');
+INSERT INTO "auth_user" ("id", "email", "password_hash", "first_name", "last_name", "phone", "is_entrpreneur", "is_admin") VALUES
+('ggf0qg7u0umvarr',	'foo@bar.com',	'$argon2id$v=19$m=19456,t=2,p=1$2wXe3uySLfiShxmVsDx7ig$Ycz29Te/viPxg/LoyFSywrsN+kUinyTtSrUehGu7qGg', 'John', 'Doe', '0815-12345',false ,false);
