b68438d72f947aa9f5baa994e0c5a5937aba351a
Steffen Heger <steffen.heger@gmail.com>
2024-07-23 14:34:57 +0200

    wip

--- booking-generator/request_gen.py
+++ booking-generator/request_gen.py
@@ -3,7 +3,7 @@ import csv
 import random
 import time
 from datetime import datetime, timedelta
-from multiprocessing import Process
+import argparse
 
 
 def generate_random_datetime(start_date, end_date):
@@ -15,18 +15,13 @@ def generate_random_datetime(start_date, end_date):
     :return: A random datetime object within the specified range.
     """
     
-    # Calculate the difference between start and end date
     delta = end_date - start_date
-    
-    # Generate a random number of days within the range
     random_days = random.randint(0, delta.days)
     
-    # Generate a random time between 8 AM and 10 PM
-    random_hour = random.randint(8, 21)  # 8 AM to 9 PM
+    random_hour = random.randint(8, 21)
     random_minute = random.randint(0, 59)
     random_second = random.randint(0, 59)
     
-    # Construct the random datetime
     random_date = start_date + timedelta(days=random_days)
     random_time = datetime(random_date.year, random_date.month, random_date.day, random_hour, random_minute, random_second)
     
@@ -36,10 +31,10 @@ def random_stop(stops):
     return stops[random.randint(0, len(stops) - 1)]
 
 
-def generate_booking_requests(url, max_passengers, start_date, end_date, n_max=None):
+def generate_booking_requests(data, url, start_date, end_date,  max_passengers, nreq, delay):
     stops = []
     
-    with open('stops.txt') as csv_file:
+    with open(data) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             stops.append(row)
@@ -80,9 +75,11 @@ def generate_booking_requests(url, max_passengers, start_date, end_date, n_max=N
                 print('Connection to server failed')
                 break
             
-            time.sleep(3)
+            time.sleep(delay)
 
-            if n_max and n == n_max:
+            print(nreq)
+            print(n)
+            if n == nreq:
                 break
             n += 1
     except KeyboardInterrupt:
@@ -90,12 +87,22 @@ def generate_booking_requests(url, max_passengers, start_date, end_date, n_max=N
 
 
 if __name__ == '__main__':
-    url = 'http://localhost:5173/api/booking'
-    range_days = 14
-    max_passengers = 3
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', required=True, help='URL of service endpoint')
+    parser.add_argument('--data', required=True, help='Path to data file (stops)')
+    parser.add_argument('--days', type=int, required=False, default=5, help='Range is NOW + days')
+    parser.add_argument('--max-passengers', type=int, required=False, default=3)
+    parser.add_argument('--nreq', type=int, required=False, help='Maximum number of requests')
+    parser.add_argument('--delay', type=int, required=False, default=1, help='Delay between requests in seconds')
+    args = parser.parse_args()
 
     now = datetime.now()
     start_date = now.date()
-    end_date = (now + timedelta(days=range_days)).date()
+    end_date = (now + timedelta(days=args.days)).date()
 
-    generate_booking_requests(url, max_passengers, start_date, end_date, n_max=10)
+    print(start_date)
+    print(end_date)
+    print(args.nreq)
+    generate_booking_requests(args.data, args.url, start_date, end_date, args.max_passengers, args.nreq, args.delay)
+
+    # use: python3 request_gen.py --url='http://localhost:5173/api/booking' --data=stops_test.txt
