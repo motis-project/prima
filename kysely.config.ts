b39670c6fa69d2ac6a035cfe4f3ac6b4fcfb2730
Felix Gündling <felix.guendling@gmail.com>
2024-08-05 10:11:01 +0200

    use $env/dynamic/private (#103)

--- kysely.config.ts
+++ kysely.config.ts
@@ -1,5 +1,12 @@
+import { PostgresDialect } from 'kysely';
 import { defineConfig } from 'kysely-ctl';
-import { dialect } from './src/lib/database';
+import pg from 'pg';
+
+export const pool = new pg.Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+export const dialect = new PostgresDialect({ pool });
 
 export default defineConfig({
 	dialect,
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,10 +1,11 @@
 import { type Database } from './types';
 import pg from 'pg';
 import { Kysely, PostgresDialect } from 'kysely';
+import { env } from '$env/dynamic/private';
 
-console.log('Connecting to dabase: ', process.env.DATABASE_URL);
+console.log('Connecting to dabase: ', env.DATABASE_URL);
 
-export const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new pg.Pool({ connectionString: env.DATABASE_URL });
 
 export const dialect = new PostgresDialect({
 	pool
