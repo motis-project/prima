8a1a04d6777f6b1838a3245799b0678a15304fb5
Steffen Heger <steffen.heger@gmail.com>
2024-08-27 11:17:28 +0200

    update test data

--- test_data/default/company.sql
+++ test_data/default/company.sql
@@ -2,9 +2,9 @@ INSERT INTO "company" ("latitude", "longitude", "name", "address", "zone", "comm
 (51.25743,	14.462836,	'Taxi-Unternehmen Bautzen-1',	'Kleindubrau Goethestraße 9',	2,	15),
 (51.10082,	14.516229,	'Taxi-Unternehmen Bautzen-2',	'Cunewalde Hauptstraße 83',	1,	10),
 (51.237354,	14.522424,	'Taxi-Unternehmen Görlitz-1',	'Malschwitz Gutauer Landstraße 15',	3,	21),
-(51.504585,	14.641439,	'Taxi Weißwasser',	'Bautzener Str. 60, 02943 Weißwasser/Oberlausitz',	2,	85),
-(51.532974,	14.660599,	'Taxi Gablenz',	'Schulstraße 21, 02953 Gablenz',	2,	41),
-(51.38096,	14.666578,	'Taxi Reichwalde',	'Robert-Koch-Straße 45, 02943 Boxberg/Oberlausitz',	3,	38),
-(51.30576,	14.782109,	'Taxi Moholz',	'Postweg 10, 02906 Niesky',	3,	65),
-(51.302185,	14.834551,	'Taxi Niesky',	'Trebuser Str. 4, 02906 Niesky',	3,	65),
-(51.321884,	14.944467,	'Taxi Rothenburg',	'Zur Wasserscheide 37, 02929 Rothenburg/Oberlausitz',	3,	75);
+(51.504585,	14.641439,	'Taxi Weißwasser',	'Bautzener Str. 60, 02943 Weißwasser/Oberlausitz',	2,	79),
+(51.532974,	14.660599,	'Taxi Gablenz',	'Schulstraße 21, 02953 Gablenz',	2,	35),
+(51.38096,	14.666578,	'Taxi Reichwalde',	'Robert-Koch-Straße 45, 02943 Boxberg/Oberlausitz',	1,	32),
+(51.30576,	14.782109,	'Taxi Moholz',	'Postweg 10, 02906 Niesky',	1,	53),
+(51.302185,	14.834551,	'Taxi Niesky',	'Trebuser Str. 4, 02906 Niesky',	1,	53),
+(51.321884,	14.944467,	'Taxi Rothenburg',	'Zur Wasserscheide 37, 02929 Rothenburg/Oberlausitz',	1,	69);
