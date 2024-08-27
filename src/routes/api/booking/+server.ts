4e7b74ab1780fbf053fbf06545fb696418addfbc
Steffen Heger <steffen.heger@gmail.com>
2024-08-27 11:21:14 +0200

    format

--- src/routes/api/booking/+server.ts
+++ src/routes/api/booking/+server.ts
@@ -178,7 +178,7 @@ export const POST = async (event) => {
 
 	console.assert(
 		Math.max(...[...mergedAvailabilites.values()].map((availabilities) => availabilities.length)) <=
-		1
+			1
 	);
 
 	const availableVehicles = [...mergedAvailabilites.entries()]
@@ -459,7 +459,11 @@ export const POST = async (event) => {
 				},
 				bestVehicle!.vehicleId
 			);
-			await db.updateTable('tour').set({ fare_route: fare_route }).where('id', '=', tourId).executeTakeFirst();
+			await db
+				.updateTable('tour')
+				.set({ fare_route: fare_route })
+				.where('id', '=', tourId)
+				.executeTakeFirst();
 		} catch (e) {
 			console.log(e);
 		}
