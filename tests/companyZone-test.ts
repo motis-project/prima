4848d863ff5772ff771a7a1ffc4da55a350d2010
Steffen Heger <steffen.heger@gmail.com>
2024-09-03 18:21:03 +0200

    format

--- tests/companyZone-test.ts
+++ tests/companyZone-test.ts
@@ -32,7 +32,9 @@ test('Set company data, incomplete 2', async ({ page }) => {
 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
 	await expect(page.getByRole('heading', { name: 'Stammdaten ihres Unternehmens' })).toBeVisible();
-	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).not.toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
 	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
@@ -54,7 +56,9 @@ test('Set company data, incomplete 3', async ({ page }) => {
 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
 	await expect(page.getByRole('heading', { name: 'Stammdaten ihres Unternehmens' })).toBeVisible();
-	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).not.toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
 	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
@@ -78,10 +82,14 @@ test('Set company data, address not in community', async ({ page }) => {
 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
 	await expect(page.getByRole('heading', { name: 'Stammdaten ihres Unternehmens' })).toBeVisible();
-	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).not.toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
-	await expect(page.getByTestId('community-error')).toHaveText('Die Addresse liegt nicht in der ausgewählten Gemeinde.');
+	await expect(page.getByTestId('community-error')).toHaveText(
+		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
+	);
 	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
 });
 
@@ -95,7 +103,9 @@ test('Set company data, complete and consistent', async ({ page }) => {
 	await expect(page.getByRole('heading', { name: 'Stammdaten ihres Unternehmens' })).toBeVisible();
 	// Login succeeded
 	await page.getByLabel('Name').fill('Test');
-	await page.getByLabel('Unternehmenssitz').fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
+	await page
+		.getByLabel('Unternehmenssitz')
+		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
 	await page.getByTestId('select-zone').click();
 	await page.getByText('Görlitz').click();
 	await page.getByTestId('select-community').click();
@@ -103,9 +113,13 @@ test('Set company data, complete and consistent', async ({ page }) => {
 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
 	await expect(page.getByRole('heading', { name: 'Stammdaten ihres Unternehmens' })).toBeVisible();
-	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).not.toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
-	await expect(page.getByTestId('community-error')).not.toHaveText('Die Addresse liegt nicht in der ausgewählten Gemeinde.');
+	await expect(page.getByTestId('community-error')).not.toHaveText(
+		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
+	);
 	await expect(page.getByText('Die Daten wurden übernommen.')).toBeVisible();
 });
