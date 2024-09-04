ccb849432fdeb3f0909b5401061f5dc9488156f1
Steffen Heger <steffen.heger@gmail.com>
2024-09-04 21:37:25 +0200

    update test data

--- test_data/default/auth_user.sql
+++ test_data/default/auth_user.sql
@@ -1,4 +1,5 @@
 INSERT INTO "auth_user" ("id", "email", "is_entrepreneur", "is_maintainer", "password_hash", "first_name", "last_name", "phone", "company_id") VALUES
+('egfrfme3qe0er5y',	'foo@bar.de',	'f',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$9fW6tfdNBJHtNC/RgNpMgg$z+hlFH7KXxKbIyt1q4fTK134FYcF8y10ZjSslzyqmFc',	'John',	'Doe',	'0815-1231234',	NULL),
 ('45rmwujnwi0sik6',	'maintainer@zvon.de',	'f',	't',	'$argon2id$v=19$m=19456,t=2,p=1$ZtuiFUoQYRyXUQRduYBkfQ$E+aREm5wKl8Ldn5ASP3wZnPf/jRriMIQmR3L3BhDaSA',	NULL,	NULL,	NULL,	NULL),
 ('r55zjskvyhdp3g1',	'taxi@weisswasser.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$BoC0z8dXsKPZmUMvpnRXPw$Hc6rK5wlUNizsw5GQFjJ9oQ9uMhgWln42Ak4J2rO8yc',	NULL,	NULL,	NULL,	4),
 ('vjewjjldyuzmfqu',	'taxi@gablenz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$3/CML3alHoFB7kYR3Fz9Hw$qQ7MYo7N6NO0SeCKXFs4VrPdiwGAT0FhE5KmwC0fv8U',	NULL,	NULL,	NULL,	5),
@@ -7,4 +8,4 @@ INSERT INTO "auth_user" ("id", "email", "is_entrepreneur", "is_maintainer", "pas
 ('ncncjn1y60mialk',	'taxi@niesky.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$jxW4oxa3l0tg+OG3+4lllw$l5TN76xuwWqc01KNBHB37WukqjmqjKsm/ZBF2y+NvPY',	NULL,	NULL,	NULL,	8),
 ('gtliidzui0k6l88',	'taxi@rothenburg.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$dviKXplqYeVGdRA+UztyDg$/rQUv5OVgKufsy6VqYtFhXfE6jaHOCV6oE+3aDZVGMo',	NULL,	NULL,	NULL,	9),
 ('fd7ji4nu49gp02t',	'taxi@schoepstal.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$B7mjUX8IFZv+1G/jiu2dSQ$xGhHcG8PKvDYLwydw2aVVqaaovdjFanlIrBjF0TgDkI',	NULL,	NULL,	NULL,	10),
-('egfrfme3qe0er5y',	'foo@bar.de',	'f',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$9fW6tfdNBJHtNC/RgNpMgg$z+hlFH7KXxKbIyt1q4fTK134FYcF8y10ZjSslzyqmFc',	'John',	'Doe',	'0815-1231234',	NULL);
+('nfdsthd4ec7tzao',	'taxi@görlitz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$6zvrI5rYSzw+NP8hRZ1Yxg$pAY9o3o3rhlCNGo2zVwP/Kq5YVOrm6yvLrqaSDeWxpw',	NULL,	NULL,	NULL,	11);
