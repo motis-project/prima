67037c140f5703b0e11c99094856007fcd70cdcc
nils_penzel <penzel.nils @web.de>
2024-09-05 14:53:39 +0200

    Change constat MAX_PASSENGER_WAITING_TIME

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
-import { hoursToMs } from './time_utils';
+import { hoursToMs, minutesToMs } from './time_utils';
 
 export const TZ = 'Europe/Berlin';
 export const MIN_PREP_MINUTES = 30;
 export const MAX_TRAVEL_DURATION = hoursToMs(1);
-export const MAX_PASSENGER_WAITING_TIME = hoursToMs(10);
+export const MAX_PASSENGER_WAITING_TIME = minutesToMs(10);
 export const SRID = 4326;
