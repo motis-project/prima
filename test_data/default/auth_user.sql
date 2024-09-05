948af99a07e422b1812e5a81599f2f5ad4950814
Steffen Heger <steffen.heger@gmail.com>
2024-09-05 10:40:50 +0200

    wip, update test data

--- test_data/default/auth_user.sql
+++ test_data/default/auth_user.sql
@@ -1,11 +1,11 @@
 INSERT INTO "auth_user" ("id", "email", "is_entrepreneur", "is_maintainer", "password_hash", "first_name", "last_name", "phone", "company_id") VALUES
 ('egfrfme3qe0er5y',	'foo@bar.de',	'f',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$9fW6tfdNBJHtNC/RgNpMgg$z+hlFH7KXxKbIyt1q4fTK134FYcF8y10ZjSslzyqmFc',	'John',	'Doe',	'0815-1231234',	NULL),
 ('45rmwujnwi0sik6',	'maintainer@zvon.de',	'f',	't',	'$argon2id$v=19$m=19456,t=2,p=1$ZtuiFUoQYRyXUQRduYBkfQ$E+aREm5wKl8Ldn5ASP3wZnPf/jRriMIQmR3L3BhDaSA',	NULL,	NULL,	NULL,	NULL),
-('r55zjskvyhdp3g1',	'taxi@weisswasser.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$BoC0z8dXsKPZmUMvpnRXPw$Hc6rK5wlUNizsw5GQFjJ9oQ9uMhgWln42Ak4J2rO8yc',	NULL,	NULL,	NULL,	4),
-('vjewjjldyuzmfqu',	'taxi@gablenz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$3/CML3alHoFB7kYR3Fz9Hw$qQ7MYo7N6NO0SeCKXFs4VrPdiwGAT0FhE5KmwC0fv8U',	NULL,	NULL,	NULL,	5),
-('s0xoswoh1pdwqsx',	'taxi@reichwalde.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$UCIZz8oGzu9kCDOpmWXxYQ$amyxen1cjPmi/TwetOz7I/f+neLvlx6eQxM6OTvIzx0',	NULL,	NULL,	NULL,	6),
-('upxzqfe4m2b0apc',	'taxi@moholz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$TCAyMLkDNz0F7nceulTs4A$+dCc3qIYwS362mcrSH/Z7hmXx2KW5Ow5NLhZH0XpPEI',	NULL,	NULL,	NULL,	7),
-('ncncjn1y60mialk',	'taxi@niesky.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$jxW4oxa3l0tg+OG3+4lllw$l5TN76xuwWqc01KNBHB37WukqjmqjKsm/ZBF2y+NvPY',	NULL,	NULL,	NULL,	8),
-('gtliidzui0k6l88',	'taxi@rothenburg.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$dviKXplqYeVGdRA+UztyDg$/rQUv5OVgKufsy6VqYtFhXfE6jaHOCV6oE+3aDZVGMo',	NULL,	NULL,	NULL,	9),
-('fd7ji4nu49gp02t',	'taxi@schoepstal.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$B7mjUX8IFZv+1G/jiu2dSQ$xGhHcG8PKvDYLwydw2aVVqaaovdjFanlIrBjF0TgDkI',	NULL,	NULL,	NULL,	10),
-('nfdsthd4ec7tzao',	'taxi@görlitz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$6zvrI5rYSzw+NP8hRZ1Yxg$pAY9o3o3rhlCNGo2zVwP/Kq5YVOrm6yvLrqaSDeWxpw',	NULL,	NULL,	NULL,	11);
+('r55zjskvyhdp3g1',	'taxi@weisswasser.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$BoC0z8dXsKPZmUMvpnRXPw$Hc6rK5wlUNizsw5GQFjJ9oQ9uMhgWln42Ak4J2rO8yc',	NULL,	NULL,	NULL,	1),
+('vjewjjldyuzmfqu',	'taxi@gablenz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$3/CML3alHoFB7kYR3Fz9Hw$qQ7MYo7N6NO0SeCKXFs4VrPdiwGAT0FhE5KmwC0fv8U',	NULL,	NULL,	NULL,	2),
+('s0xoswoh1pdwqsx',	'taxi@reichwalde.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$UCIZz8oGzu9kCDOpmWXxYQ$amyxen1cjPmi/TwetOz7I/f+neLvlx6eQxM6OTvIzx0',	NULL,	NULL,	NULL,	3),
+('upxzqfe4m2b0apc',	'taxi@moholz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$TCAyMLkDNz0F7nceulTs4A$+dCc3qIYwS362mcrSH/Z7hmXx2KW5Ow5NLhZH0XpPEI',	NULL,	NULL,	NULL,	4),
+('ncncjn1y60mialk',	'taxi@niesky.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$jxW4oxa3l0tg+OG3+4lllw$l5TN76xuwWqc01KNBHB37WukqjmqjKsm/ZBF2y+NvPY',	NULL,	NULL,	NULL,	5),
+('gtliidzui0k6l88',	'taxi@rothenburg.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$dviKXplqYeVGdRA+UztyDg$/rQUv5OVgKufsy6VqYtFhXfE6jaHOCV6oE+3aDZVGMo',	NULL,	NULL,	NULL,	6),
+('fd7ji4nu49gp02t',	'taxi@schoepstal.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$B7mjUX8IFZv+1G/jiu2dSQ$xGhHcG8PKvDYLwydw2aVVqaaovdjFanlIrBjF0TgDkI',	NULL,	NULL,	NULL,	7),
+('nfdsthd4ec7tzao',	'taxi@goerlitz.de',	't',	'f',	'$argon2id$v=19$m=19456,t=2,p=1$6zvrI5rYSzw+NP8hRZ1Yxg$pAY9o3o3rhlCNGo2zVwP/Kq5YVOrm6yvLrqaSDeWxpw',	NULL,	NULL,	NULL,	8);
