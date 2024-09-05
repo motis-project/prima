e93436603f0edd1bcc5d0af7844d0057546dc1d5
Felix Gündling <felix.guendling@gmail.com>
2024-09-05 17:28:35 +0200

    Company form rewrite (#147)
    * rewrite company form without superforms
    
    * update packages, fix bug in company page
    
    * fix linter errors

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
+	// // test for development version
+	// webServer: {
+	// 	command: 'while true; do npm run dev; done',
+	// 	url: 'http://localhost:5173',
+	// 	timeout: 10000,
+	// 	reuseExistingServer: true
+	// },
+	// use: {
+	// 	baseURL: 'http://localhost:5173/'
+	// },
 	webServer: {
 		command: 'docker compose up prima',
 		url: 'http://127.0.0.1:8080',
--- src/routes/(user)/company/+page.svelte
+++ src/routes/(user)/company/+page.svelte
@@ -1,106 +1,96 @@
 <script lang="ts">
-	const { data } = $props();
+	const { data, form } = $props();
 	import ChevronDown from 'svelte-radix/ChevronDown.svelte';
 	import * as Form from '$lib/components/ui/form';
 	import { Input } from '$lib/components/ui/input';
 	import * as Card from '$lib/components/ui/card';
-	import { formSchema } from './schema';
-	import { superForm } from 'sveltekit-superforms';
-	import { zodClient } from 'sveltekit-superforms/adapters';
-	import { enhance } from '$app/forms';
-	import { Toaster, toast } from 'svelte-sonner';
+	import { Label } from '$lib/components/ui/label/index.js';
 	import { buttonVariants } from '$lib/components/ui/button';
 	import { cn } from '$lib/utils';
+	import ExclamationTriangle from 'svelte-radix/ExclamationTriangle.svelte';
+	import * as Alert from '$lib/components/ui/alert/index.js';
 
-	const form = superForm(data.form, {
-		validators: zodClient(formSchema)
-	});
-	const { form: formData } = form;
+	let { name, address, zone, community_area } = $state(data.company!);
 </script>
-
-<Toaster />
 
 <div class="w-full h-full">
 	<Card.Header>
 		<Card.Title>Stammdaten ihres Unternehmens</Card.Title>
 	</Card.Header>
 	<Card.Content class="w-full h-full">
-		<form
-			method="POST"
-			use:enhance={() => {
-				return async ({ update }) => {
-					update({ reset: false });
-				};
-			}}
-		>
-			<div class="grid w-full grid-rows-3 grid-cols-2 gap-4">
-				<Form.Field {form} name="companyname">
-					<Form.Control let:attrs>
-						<Form.Label>Name</Form.Label>
-						<Input {...attrs} bind:value={$formData.companyname} />
-						<Form.FieldErrors />
-					</Form.Control>
-				</Form.Field>
-				<Form.Field {form} name="address">
-					<Form.Control let:attrs>
-						<Form.Label>Unternehmenssitz</Form.Label>
-						<Input {...attrs} bind:value={$formData.address} />
-						<Form.FieldErrors />
-					</Form.Control>
-				</Form.Field>
-				<Form.Field {form} name="zone">
-					<Form.Control let:attrs>
-						<Form.Label>Pflichtfahrgebiet</Form.Label>
-						<div class="relative w-full">
-							<select
-								{...attrs}
-								class={cn(
-									buttonVariants({ variant: 'outline' }),
-									'w-full appearance-none font-normal'
-								)}
-								bind:value={$formData.zone}
-							>
-								{#each data.zones as zone}
-									<option value={zone.id} selected={$formData.zone == zone.id}>
-										{zone.name.toString()}
-									</option>
-								{/each}
-							</select>
-							<ChevronDown class="absolute right-3 top-2.5 size-4 opacity-50" />
-						</div>
-						<Form.FieldErrors />
-					</Form.Control>
-				</Form.Field>
-				<Form.Field {form} name="community">
-					<Form.Control let:attrs>
-						<Form.Label>Gemeinde</Form.Label>
-						<div class="relative w-full">
-							<select
-								{...attrs}
-								class={cn(
-									buttonVariants({ variant: 'outline' }),
-									'w-full appearance-none font-normal'
-								)}
-								bind:value={$formData.community}
-							>
-								{#each data.communities as community}
-									<option value={community.id} selected={$formData.community == community.id}>
-										{community.name.toString()}
-									</option>
-								{/each}
-							</select>
-							<ChevronDown class="absolute right-3 top-2.5 size-4 opacity-50" />
-						</div>
-						<Form.FieldErrors />
-					</Form.Control>
-				</Form.Field>
-				<div class="mt-6 row-start-3 col-span-2 text-right">
-					<Form.Button
-						onclick={async () => {
-							toast(`Die Daten wurden übernommen.`);
-						}}>Übernehmen</Form.Button
-					>
+		{#if form?.error}
+			<Alert.Root variant="destructive" class="mb-4">
+				<ExclamationTriangle class="h-4 w-4" />
+				<Alert.Title>Es ist ein Fehler aufgetreten.</Alert.Title>
+				<Alert.Description>{form?.error}</Alert.Description>
+			</Alert.Root>
+		{/if}
+
+		{#if form?.success}
+			<Alert.Root class="mb-4">
+				<ExclamationTriangle class="h-4 w-4" />
+				<Alert.Title>Aktualisierung erfolgreich.</Alert.Title>
+				<Alert.Description
+					>Die Stammdaten ihres Unternehmens Ihres Unternehmens wurden aktualisiert.</Alert.Description
+				>
+			</Alert.Root>
+		{/if}
+
+		<form method="POST">
+			<div class="grid w-full grid-rows-2 grid-cols-2 gap-6">
+				<div>
+					<Label for="name">Name</Label>
+					<Input name="name" id="name" value={name} />
 				</div>
+				<div>
+					<Label for="address">Unternehmenssitz</Label>
+					<Input name="address" id="address" value={address} />
+				</div>
+				<div>
+					<Label for="zone">Pflichtfahrgebiet</Label>
+					<div class="relative w-full">
+						<select
+							name="zone"
+							id="zone"
+							class={cn(
+								buttonVariants({ variant: 'outline' }),
+								'w-full appearance-none font-normal'
+							)}
+							value={zone}
+						>
+							{#each data.zones as z}
+								<option id="zone" value={z.id} selected={zone == z.id}>
+									{z.name.toString()}
+								</option>
+							{/each}
+						</select>
+						<ChevronDown class="absolute right-3 top-2.5 size-4 opacity-50" />
+					</div>
+				</div>
+				<div>
+					<Label for="community_area">Gemeinde</Label>
+					<div class="relative w-full">
+						<select
+							name="community_area"
+							id="community_area"
+							class={cn(
+								buttonVariants({ variant: 'outline' }),
+								'w-full appearance-none font-normal'
+							)}
+							value={community_area}
+						>
+							{#each data.communities as c}
+								<option value={c.id} selected={community_area == c.id}>
+									{c.name.toString()}
+								</option>
+							{/each}
+						</select>
+						<ChevronDown class="absolute right-3 top-2.5 size-4 opacity-50" />
+					</div>
+				</div>
+			</div>
+			<div class="mt-8 w-full text-right">
+				<Form.Button>Übernehmen</Form.Button>
 			</div>
 		</form>
 	</Card.Content>
