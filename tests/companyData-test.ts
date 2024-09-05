1dc116a5f809306c94e08be9a7fd4d14633a2b06
Steffen Heger <steffen.heger@gmail.com>
2024-09-05 12:42:53 +0200

    wip

--- tests/companyData-test.ts
+++ tests/companyData-test.ts
@@ -1,107 +1,107 @@
-import { expect, test } from '@playwright/test';
-import { login, ENTREPENEUR, hammerF5 } from './testUtils';
+// import { expect, test } from '@playwright/test';
+// import { login, ENTREPENEUR, hammerF5 } from './testUtils';
 
-test('Set company data, incomplete 1', async ({ page }) => {
-	await login(page, ENTREPENEUR);
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// test('Set company data, incomplete 1', async ({ page }) => {
+// 	await login(page, ENTREPENEUR);
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
 
-	await page.getByLabel('Name').fill('Test');
-	await page.getByRole('button', { name: 'Übernehmen' }).click();
+// 	await page.getByLabel('Name').fill('Test');
+// 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
-	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).toBeVisible();
-	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
-	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
-});
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// 	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).toBeVisible();
+// 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
+// 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
+// });
 
-test('Set company data, incomplete 2', async ({ page }) => {
-	await login(page, ENTREPENEUR);
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// test('Set company data, incomplete 2', async ({ page }) => {
+// 	await login(page, ENTREPENEUR);
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
 
-	await page.getByLabel('Name').fill('Test');
-	await page.getByLabel('Unternehmenssitz').fill('Plantagenweg 3, 02827 Görlitz');
-	await page.getByRole('button', { name: 'Übernehmen' }).click();
+// 	await page.getByLabel('Name').fill('Test');
+// 	await page.getByLabel('Unternehmenssitz').fill('Plantagenweg 3, 02827 Görlitz');
+// 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
-	await expect(
-		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
-	).not.toBeVisible();
-	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
-	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
-	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
-});
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// 	await expect(
+// 		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+// 	).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
+// 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
+// 	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
+// });
 
-test('Set company data, incomplete 3', async ({ page }) => {
-	await login(page, ENTREPENEUR);
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// test('Set company data, incomplete 3', async ({ page }) => {
+// 	await login(page, ENTREPENEUR);
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
 
-	await page.getByLabel('Name').fill('Taxi Weißwasser');
-	await page
-		.getByLabel('Unternehmenssitz')
-		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
-	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
-	await page.getByRole('button', { name: 'Übernehmen' }).click();
+// 	await page.getByLabel('Name').fill('Taxi Weißwasser');
+// 	await page
+// 		.getByLabel('Unternehmenssitz')
+// 		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
+// 	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
+// 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
-	await expect(
-		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
-	).not.toBeVisible();
-	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
-	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
-	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
-});
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// 	await expect(
+// 		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+// 	).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
+// 	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
+// });
 
-test('Set company data, address not in community', async ({ page }) => {
-	await login(page, ENTREPENEUR);
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// test('Set company data, address not in community', async ({ page }) => {
+// 	await login(page, ENTREPENEUR);
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
 
-	await page.getByLabel('Name').fill('Taxi Weißwasser');
-	await page.getByLabel('Unternehmenssitz').fill('Plantagenweg 3, 02827 Görlitz');
-	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
-	await page.getByLabel('Gemeinde').selectOption({ label: 'Weißwasser/O.L.' });
-	await page.getByRole('button', { name: 'Übernehmen' }).click();
+// 	await page.getByLabel('Name').fill('Taxi Weißwasser');
+// 	await page.getByLabel('Unternehmenssitz').fill('Plantagenweg 3, 02827 Görlitz');
+// 	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
+// 	await page.getByLabel('Gemeinde').selectOption({ label: 'Weißwasser/O.L.' });
+// 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
-	await expect(
-		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
-	).not.toBeVisible();
-	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
-	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
-	await expect(page.getByTestId('community-error')).toHaveText(
-		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
-	);
-	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
-});
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// 	await expect(
+// 		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+// 	).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
+// 	await expect(page.getByTestId('community-error')).toHaveText(
+// 		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
+// 	);
+// 	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
+// });
 
-test('Set company data, complete and consistent', async ({ page }) => {
-	await login(page, ENTREPENEUR);
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// test('Set company data, complete and consistent', async ({ page }) => {
+// 	await login(page, ENTREPENEUR);
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
 
-	await page.getByLabel('Name').fill('Taxi Weißwasser');
-	await page
-		.getByLabel('Unternehmenssitz')
-		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
-	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
-	await page.getByLabel('Gemeinde').selectOption({ label: 'Weißwasser/O.L.' });
-	await page.getByRole('button', { name: 'Übernehmen' }).click();
+// 	await page.getByLabel('Name').fill('Taxi Weißwasser');
+// 	await page
+// 		.getByLabel('Unternehmenssitz')
+// 		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
+// 	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
+// 	await page.getByLabel('Gemeinde').selectOption({ label: 'Weißwasser/O.L.' });
+// 	await page.getByRole('button', { name: 'Übernehmen' }).click();
 
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
-	await expect(
-		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
-	).not.toBeVisible();
-	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
-	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
-	await expect(page.getByTestId('community-error')).not.toHaveText(
-		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
-	);
-	await expect(page.getByText('Die Daten wurden übernommen.')).toBeVisible();
+// 	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+// 	await expect(
+// 		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+// 	).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
+// 	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
+// 	await expect(page.getByTestId('community-error')).not.toHaveText(
+// 		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
+// 	);
+// 	await expect(page.getByText('Die Daten wurden übernommen.')).toBeVisible();
 
-	await hammerF5(page);
+// 	await hammerF5(page);
 
-	await expect(page.getByLabel('Name')).toHaveValue('Taxi Weißwasser');
-	await expect(page.getByLabel('Unternehmenssitz')).toHaveValue(
-		'Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz'
-	);
-	await expect(page.getByLabel('Pflichtfahrgebiet')).toHaveValue('2' /* Görlitz */);
-	await expect(page.getByLabel('Gemeinde')).toHaveValue('85' /* Weißwasser */);
-});
+// 	await expect(page.getByLabel('Name')).toHaveValue('Taxi Weißwasser');
+// 	await expect(page.getByLabel('Unternehmenssitz')).toHaveValue(
+// 		'Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz'
+// 	);
+// 	await expect(page.getByLabel('Pflichtfahrgebiet')).toHaveValue('2' /* Görlitz */);
+// 	await expect(page.getByLabel('Gemeinde')).toHaveValue('85' /* Weißwasser */);
+// });
--- tests/userSignupActivation-test.ts
+++ tests/userSignupActivation-test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 import { Kysely, PostgresDialect, sql } from 'kysely';
 import { dbConfig } from './config';
 import pg from 'pg';
-import { login, signup, MAINTAINER, ENTREPENEUR } from './testUtils';
+import { login, signup, MAINTAINER, ENTREPENEUR, hammerF5 } from './testUtils';
 
 test.describe.configure({ mode: 'serial' });
 
@@ -29,4 +29,110 @@ test('activate taxi', async ({ page }) => {
 	await page.getByLabel('Email').fill(ENTREPENEUR.email);
 	await page.getByRole('button', { name: 'Unternehmer freischalten' }).click();
 	await expect(page.getByText('Freischalten erfolgreich!')).toBeVisible();
+});
+
+
+test('Set company data, incomplete 1', async ({ page }) => {
+	await login(page, ENTREPENEUR);
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+
+	await page.getByLabel('Name').fill('Test');
+	await page.getByRole('button', { name: 'Übernehmen' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+	await expect(page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')).toBeVisible();
+	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
+	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
+});
+
+test('Set company data, incomplete 2', async ({ page }) => {
+	await login(page, ENTREPENEUR);
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+
+	await page.getByLabel('Name').fill('Test');
+	await page.getByLabel('Unternehmenssitz').fill('Plantagenweg 3, 02827 Görlitz');
+	await page.getByRole('button', { name: 'Übernehmen' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
+	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).toBeVisible();
+	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
+	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
+});
+
+test('Set company data, incomplete 3', async ({ page }) => {
+	await login(page, ENTREPENEUR);
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+
+	await page.getByLabel('Name').fill('Taxi Weißwasser');
+	await page
+		.getByLabel('Unternehmenssitz')
+		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
+	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
+	await page.getByRole('button', { name: 'Übernehmen' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
+	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
+	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).toBeVisible();
+	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
+});
+
+test('Set company data, address not in community', async ({ page }) => {
+	await login(page, ENTREPENEUR);
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+
+	await page.getByLabel('Name').fill('Taxi Weißwasser');
+	await page.getByLabel('Unternehmenssitz').fill('Plantagenweg 3, 02827 Görlitz');
+	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
+	await page.getByLabel('Gemeinde').selectOption({ label: 'Weißwasser/O.L.' });
+	await page.getByRole('button', { name: 'Übernehmen' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
+	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
+	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
+	await expect(page.getByTestId('community-error')).toHaveText(
+		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
+	);
+	// await expect(page.getByText('Die Daten wurden übernommen.')).not.toBeVisible();
+});
+
+test('Set company data, complete and consistent', async ({ page }) => {
+	await login(page, ENTREPENEUR);
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+
+	await page.getByLabel('Name').fill('Taxi Weißwasser');
+	await page
+		.getByLabel('Unternehmenssitz')
+		.fill('Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz');
+	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: 'Görlitz' });
+	await page.getByLabel('Gemeinde').selectOption({ label: 'Weißwasser/O.L.' });
+	await page.getByRole('button', { name: 'Übernehmen' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
+	await expect(
+		page.getByText('Die Eingabe muss mindestens 2 Zeichen enthalten.')
+	).not.toBeVisible();
+	await expect(page.getByText('Es wurde kein Gebiet ausgewählt.')).not.toBeVisible();
+	await expect(page.getByText('Es wurde keine Gemeinde ausgewählt.')).not.toBeVisible();
+	await expect(page.getByTestId('community-error')).not.toHaveText(
+		'Die Addresse liegt nicht in der ausgewählten Gemeinde.'
+	);
+	await expect(page.getByText('Die Daten wurden übernommen.')).toBeVisible();
+
+	await hammerF5(page);
+
+	await expect(page.getByLabel('Name')).toHaveValue('Taxi Weißwasser');
+	await expect(page.getByLabel('Unternehmenssitz')).toHaveValue(
+		'Werner-Seelenbinder-Straße 70A, 02943 Weißwasser/Oberlausitz'
+	);
+	await expect(page.getByLabel('Pflichtfahrgebiet')).toHaveValue('2' /* Görlitz */);
+	await expect(page.getByLabel('Gemeinde')).toHaveValue('85' /* Weißwasser */);
 });
