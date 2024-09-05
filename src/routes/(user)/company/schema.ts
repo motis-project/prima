5c67de4723c98342940a42663e6357a51b62895a
Felix Gündling <felix.guendling@gmail.com>
2024-09-05 13:50:05 +0200

    fix company page errors (#146)

--- src/routes/(user)/company/schema.ts
+++ src/routes/(user)/company/schema.ts
@@ -6,8 +6,8 @@ export const formSchema = z.object({
 		.min(2, { message: 'Die Eingabe muss mindestens 2 Zeichen enthalten.' })
 		.max(50, { message: 'Die Eingabe darf höchstens 50 Zeichen enthalen.' }),
 	address: z.string().min(2, { message: 'Die Eingabe muss mindestens 2 Zeichen enthalten.' }),
-	zone: z.number(),
-	community: z.number()
+	zone: z.number().gt(0, { message: 'Es muss ein Pflichtfahrgebiet ausgewählt werden.' }),
+	community: z.number().gt(0, { message: 'Es muss eine Gemeinde ausgewählt werden.' })
 });
 
 export type FormSchema = typeof formSchema;
