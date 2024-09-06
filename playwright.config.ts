8e2f31af04a257187755ba7a7b691ee139b9a22d
Felix Gündling <felix.guendling@gmail.com>
2024-09-06 10:37:50 +0200

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	// test for development version
-	webServer: {
-		command: 'while true; do npm run dev; done',
-		url: 'http://localhost:5173',
-		timeout: 10000,
-		reuseExistingServer: true
-	},
-	use: {
-		baseURL: 'http://localhost:5173/'
-	},
 	// webServer: {
-	// 	command: 'docker compose up prima',
-	// 	url: 'http://127.0.0.1:8080',
+	// 	command: 'while true; do npm run dev; done',
+	// 	url: 'http://localhost:5173',
 	// 	timeout: 10000,
 	// 	reuseExistingServer: true
 	// },
 	// use: {
-	// 	baseURL: 'http://localhost:8080/'
+	// 	baseURL: 'http://localhost:5173/'
 	// },
+	webServer: {
+		command: 'docker compose up prima',
+		url: 'http://127.0.0.1:8080',
+		timeout: 10000,
+		reuseExistingServer: true
+	},
+	use: {
+		baseURL: 'http://localhost:8080/'
+	},
 	testDir: './tests',
 	projects: [
 		{
