fd33604e30c7e6ac504659fb5ca2674c4086c42e
nilspenzel <penzel.nils@web.de>
2024-09-09 13:05:27 +0200

    wip

--- tests/blacklist.test.ts
+++ tests/blacklist.test.ts
@@ -1,10 +1,29 @@
+import { blacklisting } from '$lib/api';
+import { Coordinates } from '$lib/location';
 import { expect, test } from '@playwright/test';
 
-test('Set company data, incomplete 1', async ({ page }) => {
-	await expect(page.getByRole('heading', { name: 'Stammdaten Ihres Unternehmens' })).toBeVisible();
-
-	await page.getByLabel('Name').fill('Test');
-	await page.getByRole('button', { name: 'Übernehmen' }).click();
-
-	await expect(page.getByText('Adresse zu kurz.')).toBeVisible();
+test('Set company data, incomplete 1', async ({ request }) => {
+	const r = {
+		userChosen: new Coordinates(51.03485947001579, 13.293447066879821),
+		busStops: [],
+		startFixed: true,
+		timeStamps: [],
+		numPassengers: 1,
+		numWheelchairs: 0,
+		numBikes: 0,
+		luggage: 0
+	};
+	const res = await blacklisting(r);
+	console.log(res);
+	expect(res.ok).toBe(false);
 });
+/*
+	userChosen: Coordinates;
+	busStops: Coordinates[];
+	startFixed: boolean;
+	timeStamps: Date[][];
+	numPassengers: number;
+	numWheelchairs: number;
+	numBikes: number;
+	luggage: number;
+	*/
