8363655b5473769da3a0fe5b5396033860a633fa
CmdrTMir <stardust.tessa@gmail.com>
2024-09-17 18:42:31 +0200

    wip pagination

--- src/routes/(maintainer)/accounting/+page.svelte
+++ src/routes/(maintainer)/accounting/+page.svelte
@@ -2,6 +2,11 @@
 	import * as Card from '$lib/components/ui/card';
 	import * as Table from '$lib/components/ui/table/index.js';
 	import { type TourDetails } from '$lib/TourDetails.js';
+	import ChevronLeft from "lucide-svelte/icons/chevron-left";
+    import ChevronRight from "lucide-svelte/icons/chevron-right";
+	import { Button } from '$lib/components/ui/button';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import { onMount } from "svelte";
 
     const { data } = $props();
 
@@ -12,6 +17,35 @@
 		});
 		return customers.size;
 	};
+	
+	let rows = [];
+	let page = $state(0);
+	let perPage = 2;
+	let firstPage = data.tours.slice(0, perPage);
+	let firstarray = [firstPage];
+	let totalPages = $state(firstarray);
+	let currentPageRows = $state(firstPage);
+	
+	const paginate = (tours: TourDetails[]) => {
+		const pagesCount = Math.ceil(tours.length / perPage);
+		const paginatedItems = Array.from({ length: pagesCount }, (_, index) => {
+			const start = index * perPage;
+			return tours.slice(start, start + perPage);
+			});
+		totalPages = [...paginatedItems];
+  	};
+	
+	onMount(() => {
+		rows = data.tours;
+    	paginate(rows);
+  	});
+	
+	const setPage = (p: number) => {
+		if (p >= 0 && p < totalPages.length) {
+			page = p;
+		}
+		currentPageRows = totalPages.length > 0 ? totalPages[page] : [];
+	}
 </script>
 
 <div class="w-full h-full">
@@ -32,7 +66,7 @@
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
-				{#each data.tours as tour}
+				{#each currentPageRows as tour}
 					<Table.Row>
 						<Table.Cell>{tour.company_name}</Table.Cell>
 						<Table.Cell>{tour.from.toLocaleString('de-DE').slice(0, -3)}</Table.Cell>
@@ -46,4 +80,37 @@
 			</Table.Body>
 		</Table.Root>
 	</Card.Content>
+</div>
+<div>
+	<Button variant="outline" on:click={() => setPage(page - 1)}>
+		<ChevronLeft class="h-4 w-4" />
+		Vorherige Seite
+	</Button>
+	{#if totalPages.length > 5}
+		{#each {length: 2} as _, i}
+			<Button variant="outline" on:click={() => setPage(i)}>
+				{i + 1}
+			</Button>	
+		{/each}
+		...
+		<Button variant="outline" on:click={() => setPage(totalPages.length-2)}>
+			{totalPages.length-1}
+		</Button>	
+		<Button variant="outline" on:click={() => setPage(totalPages.length-1)}>
+			{totalPages.length}
+		</Button>	
+	{:else}
+		{#each totalPages as page, i}
+			<Button variant="outline" on:click={() => setPage(i)}>
+				{i + 1}
+			</Button>		
+		{/each}
+	{/if}
+	<Button variant="outline" on:click={() => setPage(page + 1)}>
+		Nächste Seite
+		<ChevronRight class="h-4 w-4" />
+	</Button>
+	<Label>
+		Auf Seite {page+1} von {totalPages.length}
+	</Label>
 </div>
