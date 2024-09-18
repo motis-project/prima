6939c3b9eda0302b17efd59122607a2060e5d32c
CmdrTMir <stardust.tessa@gmail.com>
2024-09-18 14:03:20 +0200

    wip, price calculation

--- src/routes/(maintainer)/accounting/+page.svelte
+++ src/routes/(maintainer)/accounting/+page.svelte
@@ -4,6 +4,7 @@
 	import { type TourDetails } from '$lib/TourDetails.js';
 	import ChevronLeft from "lucide-svelte/icons/chevron-left";
     import ChevronRight from "lucide-svelte/icons/chevron-right";
+	import { ChevronsRight, ChevronsLeft, ChevronsUpDown } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	import Label from '$lib/components/ui/label/label.svelte';
 	import { onMount } from "svelte";
@@ -17,10 +18,25 @@
 		});
 		return customers.size;
 	};
+
+	// fare = ÖV-Preis  ---  fare_route = Taxameterpreis
+	const getTotalPrice = (fare: number | null, fare_route: number | null) => {
+		if(fare == null || fare_route == null) {
+			return 0;
+		}
+		return fare + fare_route;
+	};
+
+	const getCost = (fare: number | null, fare_route: number | null) => {
+		if(fare == null || fare_route == null) {
+			return 0;
+		}
+		return (fare_route - fare) * 0.97;
+	};
 	
 	let rows = [];
 	let page = $state(0);
-	let perPage = 2;
+	let perPage = 5;
 	let firstPage = data.tours.slice(0, perPage);
 	let firstarray = [firstPage];
 	let totalPages = $state(firstarray);
@@ -46,6 +62,10 @@
 		}
 		currentPageRows = totalPages.length > 0 ? totalPages[page] : [];
 	}
+
+	const sort = (des: boolean) => {
+
+	}
 </script>
 
 <div class="w-full h-full">
@@ -56,13 +76,23 @@
 		<Table.Root>
 			<Table.Header>
 				<Table.Row>
-					<Table.Head>Unternehmen</Table.Head>
-					<Table.Head>Abfahrt</Table.Head>
-					<Table.Head>Ankunft</Table.Head>
-					<Table.Head>Anzahl Kunden</Table.Head>
-					<Table.Head>Taxameterpreis</Table.Head>
-                    <Table.Head>ÖV-Preis</Table.Head>
-                    <Table.Head>Preis</Table.Head>
+					<Table.Head class="mt-6.5">Unternehmen</Table.Head>
+					<Table.Head class="mt-6 flex justify-start">
+						Abfahrt
+						<ChevronsUpDown class="mx-2 size-5" on:click={() => sort(true)}></ChevronsUpDown>
+					</Table.Head>
+					<Table.Head class="mt-6.5">Ankunft</Table.Head>
+					<Table.Head class="mt-6.5">Anzahl Kunden</Table.Head>
+					<Table.Head class="mt-6.5">Taxameterpreis</Table.Head>
+                    <Table.Head class="mt-6.5">ÖV-Preis</Table.Head>
+                    <Table.Head class="mt-6 flex justify-start">
+						Gesamtpreis
+						<ChevronsUpDown class="mx-2 size-5" on:click={() => sort(true)}></ChevronsUpDown>
+					</Table.Head>
+					<Table.Head class="mt-6">
+						Kosten
+						<ChevronsUpDown class="mx-2 size-5" on:click={() => sort(true)}></ChevronsUpDown>
+					</Table.Head>
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
@@ -72,45 +102,50 @@
 						<Table.Cell>{tour.from.toLocaleString('de-DE').slice(0, -3)}</Table.Cell>
 						<Table.Cell>{tour.to.toLocaleString('de-DE').slice(0, -3)}</Table.Cell>
 						<Table.Cell>{getCustomerCount(tour)}</Table.Cell>
-						<Table.Cell>TODO</Table.Cell>
-                        <Table.Cell>TODO</Table.Cell>
-                        <Table.Cell>TODO</Table.Cell>
+						<Table.Cell>{tour.fare_route} €</Table.Cell>
+                        <Table.Cell>{tour.fare} €</Table.Cell>
+                        <Table.Cell>{getTotalPrice(tour.fare, tour.fare_route)} €</Table.Cell>
+						<Table.Cell>{getCost(tour.fare, tour.fare_route)} €</Table.Cell>
 					</Table.Row>
 				{/each}
 			</Table.Body>
 		</Table.Root>
 	</Card.Content>
 </div>
-<div>
-	<Button variant="outline" on:click={() => setPage(page - 1)}>
-		<ChevronLeft class="h-4 w-4" />
-		Vorherige Seite
-	</Button>
-	{#if totalPages.length > 5}
-		{#each {length: 2} as _, i}
-			<Button variant="outline" on:click={() => setPage(i)}>
-				{i + 1}
-			</Button>	
-		{/each}
-		...
-		<Button variant="outline" on:click={() => setPage(totalPages.length-2)}>
-			{totalPages.length-1}
+<div class="flex justify-center">
+	{#if totalPages.length > 10}
+		<Button variant="outline" on:click={() => setPage(0)}>
+			<ChevronsLeft class="mx-1 h-4 w-4" />
+			Erste Seite
 		</Button>	
+		<Button variant="outline" on:click={() => setPage(page - 1)}>
+			<ChevronLeft class="h-4 w-4" />
+			Vorherige
+		</Button>
+		<Button variant="outline" on:click={() => setPage(page + 1)}>
+			Nächste
+			<ChevronRight class="h-4 w-4" />
+		</Button>
 		<Button variant="outline" on:click={() => setPage(totalPages.length-1)}>
-			{totalPages.length}
+			Letzte Seite
+			<ChevronsRight class="mx-1 h-4 w-4" />
 		</Button>	
 	{:else}
+		<Button variant="outline" on:click={() => setPage(page - 1)}>
+			<ChevronLeft class="h-4 w-4" />
+			Vorherige Seite
+		</Button>
 		{#each totalPages as page, i}
 			<Button variant="outline" on:click={() => setPage(i)}>
 				{i + 1}
 			</Button>		
 		{/each}
+		<Button variant="outline" on:click={() => setPage(page + 1)}>
+			Nächste Seite
+			<ChevronRight class="h-4 w-4" />
+		</Button>
 	{/if}
-	<Button variant="outline" on:click={() => setPage(page + 1)}>
-		Nächste Seite
-		<ChevronRight class="h-4 w-4" />
-	</Button>
-	<Label>
+	<Label class="mx-2 mt-2.5">
 		Auf Seite {page+1} von {totalPages.length}
 	</Label>
 </div>
