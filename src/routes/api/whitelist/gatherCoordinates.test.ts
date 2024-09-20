d326dc5cc64892faabe19d751df43bfe7f4b5833
nils <penzel.nils@web.de>
2024-09-20 10:34:11 +0200

    wip

--- src/routes/api/whitelist/gatherCoordinates.test.ts
+++ src/routes/api/whitelist/gatherCoordinates.test.ts
@@ -81,9 +81,34 @@ describe('gather coordinates test', () => {
 		insertions.set(2, [{ earliestPickup: 0, latestDropoff: 2 }]);
 		const coordinates = gatherRoutingCoordinates(companies, busStops, insertions);
 		expect(coordinates.busStopMany).toHaveLength(2);
-		expect(coordinates.userChosenMany).toHaveLength(18);
+		expect(coordinates.userChosenMany).toHaveLength(6);
 		busStops.forEach((_, idx) => {
-			expect(coordinates.busStopMany[idx]).toHaveLength(18);
+			expect(coordinates.busStopMany[idx]).toHaveLength(6);
 		});
+		// Company coordinates
+		expect(coordinates.userChosenMany[0].lat).toBe(5);
+		expect(coordinates.busStopMany[0][0].lat).toBe(5);
+		expect(coordinates.busStopMany[1][0].lat).toBe(5);
+		expect(coordinates.userChosenMany[3].lat).toBe(6);
+		expect(coordinates.busStopMany[0][3].lat).toBe(6);
+		expect(coordinates.busStopMany[1][3].lat).toBe(6);
+
+		// Event coordinates
+		expect(coordinates.userChosenMany[1].lat).toBe(100);
+		expect(coordinates.busStopMany[0][1].lat).toBe(100);
+		expect(coordinates.busStopMany[1][1].lat).toBe(100);
+
+		expect(coordinates.userChosenMany[2].lat).toBe(101);
+		expect(coordinates.busStopMany[0][2].lat).toBe(101);
+		expect(coordinates.busStopMany[1][2].lat).toBe(101);
+
+		expect(coordinates.userChosenMany[4].lat).toBe(102);
+		expect(coordinates.busStopMany[0][4].lat).toBe(102);
+		expect(coordinates.busStopMany[1][4].lat).toBe(102);
+
+		expect(coordinates.userChosenMany[5].lat).toBe(103);
+		expect(coordinates.busStopMany[0][5].lat).toBe(103);
+		expect(coordinates.busStopMany[1][5].lat).toBe(103);
+
 	});
 });
--- src/routes/api/whitelist/routing.ts
+++ src/routes/api/whitelist/routing.ts
@@ -50,7 +50,7 @@ export function gatherRoutingCoordinates(
 		.flatMap((c) => c.vehicles)
 		.flatMap((v) => insertionsByVehicle.get(v.id)!)
 		.reduce((sum, current) => sum + current.latestDropoff - current.earliestPickup, 0);
-	const insertionCount = (companies.length + eventInsertionCount) * (busStops.length + 1);
+	const insertionCount = companies.length + eventInsertionCount;
 	const busStopMany = new Array<Coordinates[]>(busStops.length);
 	for (let i = 0; i != busStopMany.length; ++i) {
 		busStopMany[i] = new Array<Coordinates>(insertionCount);
@@ -61,27 +61,21 @@ export function gatherRoutingCoordinates(
 		insertionsByVehicle,
 		(company, companyPos) => {
 			for (let busStopIdx = 0; busStopIdx != busStops.length; ++busStopIdx) {
-				busStopMany[busStopIdx][companyPos++] = company.coordinates;
-				console.log('companyPos: ', companyPos);
+				busStopMany[busStopIdx][companyPos] = company.coordinates;
 			}
 			userChosenMany[companyPos++] = company.coordinates;
-			console.log('companyPos: ', companyPos);
 			return companyPos;
 		},
 		(_) => {},
 		(events, insertionIdx, _, eventPos) => {
 			const eventCoordinates = events[insertionIdx].coordinates;
 			for (let busStopIdx = 0; busStopIdx != busStops.length; ++busStopIdx) {
-				busStopMany[busStopIdx][eventPos++] = eventCoordinates;
-				console.log('eventPos: ', eventPos);
+				busStopMany[busStopIdx][eventPos] = eventCoordinates;
 			}
 			userChosenMany[eventPos++] = eventCoordinates;
-			console.log('eventPos: ', eventPos);
 			return eventPos;
 		}
 	);
-	console.log(insertionCount);
-	console.log('user', userChosenMany);
 	return {
 		busStopMany,
 		userChosenMany
