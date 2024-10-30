508ec7eb245c140f2530b0b5c6beb19f35c4477f
CmdrTMir <stardust.tessa@gmail.com>
2024-10-30 10:50:13 +0100

    all accounting changes into csv-export-branch

--- src/lib/CompletedTours.svelte
+++ src/lib/CompletedTours.svelte
@@ -3,6 +3,12 @@
 	import * as Table from '$lib/components/ui/table/index.js';
 	import { getTourInfoShort, type TourDetails } from '$lib/TourDetails.js';
 	import TourDialog from '$lib/TourDialog.svelte';
+	import { onMount } from 'svelte';
+	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
+	import { ChevronsRight, ChevronsLeft } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
+	import Label from '$lib/components/ui/label/label.svelte';
 
 	type Props = {
 		isMaintainer: boolean;
@@ -20,6 +26,43 @@
 			customers.add(e.customer_id!);
 		});
 		return customers.size;
+	};
+
+	// --- Pageination ---
+	let currentRows: TourDetails[] = [];
+	let page = $state(0);
+	let perPage = 15;
+	let firstPage = tours.slice(0, perPage);
+	let firstarray = [firstPage];
+	let totalPages = $state(firstarray);
+	let currentPageRows = $state(firstPage);
+
+	const paginate = (tours: TourDetails[]) => {
+		const pagesCount = Math.ceil(tours.length / perPage);
+		const paginatedItems = Array.from({ length: pagesCount }, (_, index) => {
+			const start = index * perPage;
+			return tours.slice(start, start + perPage);
+		});
+		totalPages = [...paginatedItems];
+	};
+
+	onMount(() => {
+		currentRows = tours;
+		paginate(currentRows);
+	});
+
+	const setPage = (p: number) => {
+		if (p >= 0 && p < totalPages.length) {
+			page = p;
+		}
+		currentPageRows = totalPages.length > 0 ? totalPages[page] : [];
+	};
+
+	const getTotalPrice = (fare: number | null, fare_route: number | null) => {
+		if (fare == null || fare_route == null) {
+			return 0;
+		}
+		return (Math.round(fare + fare_route) / 100).toFixed(2);
 	};
 </script>
 
@@ -47,7 +90,7 @@
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
-				{#each tours as tour}
+				{#each currentPageRows as tour}
 					<Table.Row
 						on:click={() => {
 							selectedTour = { tours: [tour] };
@@ -66,12 +109,50 @@
 						<Table.Cell>{getCustomerCount(tour)}</Table.Cell>
 						<Table.Cell>TODO</Table.Cell>
 						<Table.Cell>{tour.events.length}</Table.Cell>
-						<Table.Cell>TODO</Table.Cell>
+						<Table.Cell>{getTotalPrice(tour.fare, tour.fare_route)} €</Table.Cell>
 					</Table.Row>
 				{/each}
 			</Table.Body>
 		</Table.Root>
 	</Card.Content>
+</div>
+
+<div class="flex justify-center">
+	{#if totalPages.length > 10}
+		<Button variant="outline" on:click={() => setPage(0)}>
+			<ChevronsLeft class="mx-1 h-4 w-4" />
+			Erste Seite
+		</Button>
+		<Button variant="outline" on:click={() => setPage(page - 1)}>
+			<ChevronLeft class="h-4 w-4" />
+			Vorherige
+		</Button>
+		<Button variant="outline" on:click={() => setPage(page + 1)}>
+			Nächste
+			<ChevronRight class="h-4 w-4" />
+		</Button>
+		<Button variant="outline" on:click={() => setPage(totalPages.length - 1)}>
+			Letzte Seite
+			<ChevronsRight class="mx-1 h-4 w-4" />
+		</Button>
+	{:else}
+		<Button variant="outline" on:click={() => setPage(page - 1)}>
+			<ChevronLeft class="h-4 w-4" />
+			Vorherige Seite
+		</Button>
+		{#each totalPages as _page, i}
+			<Button variant="outline" on:click={() => setPage(i)}>
+				{i + 1}
+			</Button>
+		{/each}
+		<Button variant="outline" on:click={() => setPage(page + 1)}>
+			Nächste Seite
+			<ChevronRight class="h-4 w-4" />
+		</Button>
+	{/if}
+	<Label class="mx-2 mt-2.5">
+		Auf Seite {page + 1} von {totalPages.length}
+	</Label>
 </div>
 
 <TourDialog bind:open={selectedTour} />
