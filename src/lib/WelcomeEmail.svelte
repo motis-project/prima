e0ea3de9ac88aad6361e3abb3131f5dd77afdd5a
CmdrTMir <stardust.tessa@gmail.com>
2024-11-22 14:45:29 +0100

    wip send welcome email

---
+++ src/lib/WelcomeEmail.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Button, Hr, Html, Text } from 'svelte-email';
+
+	export let name = 'World';
+</script>
+
+<Html lang="en">
+	<Text>
+		Hello, {name}!
+	</Text>
+	<Hr />
+	<Button href="https://svelte.dev">Visit Svelte</Button>
+</Html>
