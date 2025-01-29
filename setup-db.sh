374e1a76cd53cd01f0274f7765edfc9ed6089e24
Felix Gündling <felix.guendling@gmail.com>
2025-01-29 14:17:39 +0100

    wip

--- setup-db.sh
+++ setup-db.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-export DATABASE_URL="postgresql://postgres:pw@localhost:6500"
+export DB="postgresql://postgres:pw@localhost:6500/"
+export DATABASE_URL="postgresql://postgres:pw@localhost:6500/prima"
 
-# docker compose up -d pg
+docker compose up -d pg
 
-# sleep 2
+sleep 2
 
-# echo "DROP DATABASE prima;" | psql $DATABASE_URL
-# echo "CREATE DATABASE prima;" | psql $DATABASE_URL
+echo "DROP DATABASE prima;" | psql $DB
+echo "CREATE DATABASE prima;" | psql $DB
 
-# pnpm run kysely migrate:latest
+pnpm run kysely migrate:latest
 
-psql $DATABASE_URL/prima --user postgres < data/address.sql
-psql $DATABASE_URL/prima --user postgres < data/zone.sql
-psql $DATABASE_URL/prima --user postgres < data/company.sql
-psql $DATABASE_URL/prima --user postgres < data/vehicle.sql
-psql $DATABASE_URL/prima --user postgres < data/availability.sql
-psql $DATABASE_URL/prima --user postgres < data/tour.sql
-psql $DATABASE_URL/prima --user postgres < data/user.sql
-psql $DATABASE_URL/prima --user postgres < data/event.sql
+psql $DATABASE_URL --user postgres < data/address.sql
+psql $DATABASE_URL --user postgres < data/zone.sql
+psql $DATABASE_URL --user postgres < data/company.sql
+psql $DATABASE_URL --user postgres < data/vehicle.sql
+psql $DATABASE_URL --user postgres < data/availability.sql
+psql $DATABASE_URL --user postgres < data/tour.sql
+psql $DATABASE_URL --user postgres < data/user.sql
+psql $DATABASE_URL --user postgres < data/event.sql
