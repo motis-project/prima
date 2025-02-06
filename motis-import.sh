a1797925e88f91839c27fe01e931501e53521350
Felix Gündling <felix.guendling@gmail.com>
2025-02-06 13:50:16 +0100

    wip

--- motis-import.sh
+++ motis-import.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-wget -nc "https://www.opendata-oepnv.de/index.php?id=1384&tx_vrrkit_view%5Bsharing%5D=eyJkYXRhc2V0IjoiZGV1dHNjaGxhbmR3ZWl0ZS1zb2xsZmFocnBsYW5kYXRlbi1ndGZzIiwidXNlcklkIjo4OH0%3D&tx_vrrkit_view%5Baction%5D=download&tx_vrrkit_view%5Bcontroller%5D=View" -O GTFS.zip
-wget -nc "https://download.geofabrik.de/europe/germany/sachsen-latest.osm.pbf" -O osm.pbf
+wget -nc "https://www.opendata-oepnv.de/index.php?id=1384&tx_vrrkit_view%5Bsharing%5D=eyJkYXRhc2V0IjoiZGV1dHNjaGxhbmR3ZWl0ZS1zb2xsZmFocnBsYW5kYXRlbi1ndGZzIiwidXNlcklkIjo4OH0%3D&tx_vrrkit_view%5Baction%5D=download&tx_vrrkit_view%5Bcontroller%5D=View" -O $HOME/GTFS.zip
+wget -nc "https://download.geofabrik.de/europe/germany/sachsen-latest.osm.pbf" -O $HOME/osm.pbf
+
+rm osm.pbf
+rm GTFS.zip
+
+ln -s $HOME/osm.pbf osm.pbf
+ln -s $HOME/GTFS.zip GTFS.zip
 
 docker compose up motis-import
