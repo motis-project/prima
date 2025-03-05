6784a1241c416dc0559c2abafb14193332c0d46f
nils penzel <penzel.nils@web.de>
2025-03-05 11:48:28 +0100

    wip

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -88,6 +88,7 @@ export async function setCompanyData(page: Page, user: UserCredentials, company:
 
 	await page.getByLabel('Name').fill(company.name);
 	await page.getByLabel('Unternehmenssitz').pressSequentially(company.address, { delay: 10 });
+	await page.screenshot({ path: 'screenshots/duringSetCompany.png', fullPage: true });
 	await page.getByText('Werner-Seelenbinder-Straße 70a').click();
 
 	await page.getByLabel('Pflichtfahrgebiet').selectOption({ label: company.zone });
