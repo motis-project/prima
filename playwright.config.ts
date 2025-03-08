c49ddcca652f71d1f26c7beae3114c3986b64bb6
Traines <git@traines.eu>
2025-03-08 02:07:14 +0000

    increase playwright timeout (?)

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run dev',
 		url: 'http://localhost:5173',
-		timeout: 20000,
+		timeout: 60000,
 		reuseExistingServer: true
 	},
 	use: {
