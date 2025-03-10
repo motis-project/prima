cd694854d2cfa6c29c99266bd480bbd98e6d1ff1
nils <penzel.nils@web.de>
2025-03-10 12:23:10 +0100

    wip
    	closes#200

---
+++ src/lib/server/email/EmailFooter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { ORIGIN } from '$env/static/private';
+	import { PUBLIC_PROVIDER } from '$env/static/public';
+</script>
+
+<p>
+	Liebe Grüße<br />
+	Dein {PUBLIC_PROVIDER} Team<br />
+	<a href={ORIGIN} target="_blank">{ORIGIN}</a><br />
+</p>
