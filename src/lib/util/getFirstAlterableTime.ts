1814368fdca52350687e7742f62cba520b6875e5
nils penzel <penzel.nils@web.de>
2025-03-12 12:03:36 +0100

    wip

--- src/lib/util/getFirstAlterableTime.ts
+++ src/lib/util/getFirstAlterableTime.ts
@@ -1,6 +1,6 @@
-import { MIN_PREP } from "$lib/constants";
-import { MINUTE } from "./time";
+import { MIN_PREP } from '$lib/constants';
+import { MINUTE } from './time';
 
 export function getFirstAlterableTime() {
-    return Math.ceil((Date.now() + MIN_PREP) / (15 * MINUTE)) * 15 * MINUTE;
+	return Math.ceil((Date.now() + MIN_PREP) / (15 * MINUTE)) * 15 * MINUTE;
 }
