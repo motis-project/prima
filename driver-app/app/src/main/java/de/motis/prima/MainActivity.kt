4eadd8ac3deec1f2e7cb2682c98726181d631f11
Steffen Heger <steffen.heger@gmail.com>
2025-03-15 18:04:00 +0100

    wip

--- driver-app/app/src/main/java/de/motis/prima/MainActivity.kt
+++ driver-app/app/src/main/java/de/motis/prima/MainActivity.kt
@@ -1,8 +1,11 @@
 package de.motis.prima
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +14,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { Nav() }
+        requestPermissions()
+    }
+
+    private val permissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissions[Manifest.permission.POST_NOTIFICATIONS] ?: false
+        }
+
+        //permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false
+        //permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false
+        val cameraGranted = permissions[Manifest.permission.CAMERA] ?: false
+
+        if (cameraGranted) {
+            setContent { Nav() }
+        }
+    }
+
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissionsToRequest.add(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        permissionsLauncher.launch(permissionsToRequest.toTypedArray())
     }
 }
