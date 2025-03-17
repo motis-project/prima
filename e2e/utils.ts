c5c80efc32df79e32f0b9caccca7636296cf0b5e
mority <mority@posteo.net>
2025-03-17 15:01:22 +0100

    use nowOrSimulationTime

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -2,13 +2,13 @@ import { test, expect, type Page } from '@playwright/test';
 import { Kysely, PostgresDialect, RawBuilder, sql } from 'kysely';
 import { dbConfig } from './config';
 import pg from 'pg';
-import { DAY, HOUR, MINUTE } from '../src/lib/util/time';
+import { DAY, HOUR, MINUTE, nowOrSimulationTime } from '../src/lib/util/time';
 import { LOCALE } from '../src/lib/constants';
 import { getOffset } from '../src/lib/util/getOffset';
 
 test.use({ locale: LOCALE });
 
-export const in6Days = new Date(Math.ceil(Date.now() / DAY) * DAY + 5 * DAY);
+export const in6Days = new Date(Math.ceil(nowOrSimulationTime().getTime() / DAY) * DAY + 5 * DAY);
 export const dayString = in6Days.toISOString().split('T')[0];
 export const offset = getOffset(in6Days.getTime() + HOUR * 12) / MINUTE;
 export type UserCredentials = {
