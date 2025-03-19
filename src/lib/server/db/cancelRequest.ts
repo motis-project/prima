cb1dab84821b7ce5718f8c741a4af8416608aeb1
nils <penzel.nils@web.de>
2025-03-19 13:46:23 +0100

    Cancel Requests/Tours inside of a transaction

--- src/lib/server/db/cancelRequest.ts
+++ src/lib/server/db/cancelRequest.ts
@@ -5,63 +5,66 @@ import { sendMail } from '$lib/server/sendMail';
 import CancelNotificationCompany from '$lib/server/email/CancelNotificationCompany.svelte';
 
 export const cancelRequest = async (requestId: number, userId: number) => {
-	const tour = await db
-		.selectFrom('request as cancelled_request')
-		.where('cancelled_request.id', '=', requestId)
-		.innerJoin('tour', 'tour.id', 'cancelled_request.tour')
-		.select((eb) => [
-			'tour.id',
-			'tour.departure',
-			'cancelled_request.ticketChecked',
-			jsonArrayFrom(
-				eb
-					.selectFrom('request as cancelled_request')
-					.innerJoin('tour as cancelled_tour', 'cancelled_tour.id', 'cancelled_request.tour')
-					.innerJoin('request', 'request.tour', 'cancelled_tour.id')
-					.innerJoin('event', 'event.request', 'request.id')
-					.where('cancelled_request.id', '=', requestId)
-					.select([
-						'event.address',
-						'event.scheduledTimeStart',
-						'event.scheduledTimeEnd',
-						'event.cancelled',
-						'cancelled_tour.id as tourid'
-					])
-			).as('events'),
-			jsonArrayFrom(
-				eb
-					.selectFrom('request')
-					.innerJoin('tour', 'tour.id', 'request.tour')
-					.innerJoin('vehicle', 'vehicle.id', 'tour.vehicle')
-					.innerJoin('company', 'company.id', 'vehicle.company')
-					.innerJoin('user', 'user.companyId', 'company.id')
-					.where('request.id', '=', requestId)
-					.where('user.isTaxiOwner', '=', true)
-					.select(['user.name', 'user.email'])
-			).as('companyOwners')
-		])
-		.executeTakeFirst();
-	if (tour === undefined) {
-		return;
-	}
-	if (tour.ticketChecked === true) {
-		return;
-	}
-	await sql`CALL cancel_request(${requestId}, ${userId}, ${Date.now()})`.execute(db);
-	for (const companyOwner of tour.companyOwners) {
-		try {
-			await sendMail(CancelNotificationCompany, 'Stornierte Buchung', companyOwner.email, {
-				events: tour.events,
-				name: companyOwner.name,
-				departure: tour.departure
-			});
-		} catch {
-			console.log(
-				'Failed to send cancellation email to company with email: ',
-				companyOwner.email,
-				' tourId: ',
-				tour.id
-			);
+	await db.transaction().execute(async (trx) => {
+		await sql`LOCK TABLE tour, request, event, "user" IN ACCESS EXCLUSIVE MODE;`.execute(trx);
+		const tour = await trx
+			.selectFrom('request as cancelled_request')
+			.where('cancelled_request.id', '=', requestId)
+			.innerJoin('tour', 'tour.id', 'cancelled_request.tour')
+			.select((eb) => [
+				'tour.id',
+				'tour.departure',
+				'cancelled_request.ticketChecked',
+				jsonArrayFrom(
+					eb
+						.selectFrom('request as cancelled_request')
+						.innerJoin('tour as cancelled_tour', 'cancelled_tour.id', 'cancelled_request.tour')
+						.innerJoin('request', 'request.tour', 'cancelled_tour.id')
+						.innerJoin('event', 'event.request', 'request.id')
+						.where('cancelled_request.id', '=', requestId)
+						.select([
+							'event.address',
+							'event.scheduledTimeStart',
+							'event.scheduledTimeEnd',
+							'event.cancelled',
+							'cancelled_tour.id as tourid'
+						])
+				).as('events'),
+				jsonArrayFrom(
+					eb
+						.selectFrom('request')
+						.innerJoin('tour', 'tour.id', 'request.tour')
+						.innerJoin('vehicle', 'vehicle.id', 'tour.vehicle')
+						.innerJoin('company', 'company.id', 'vehicle.company')
+						.innerJoin('user', 'user.companyId', 'company.id')
+						.where('request.id', '=', requestId)
+						.where('user.isTaxiOwner', '=', true)
+						.select(['user.name', 'user.email'])
+				).as('companyOwners')
+			])
+			.executeTakeFirst();
+		if (tour === undefined) {
+			return;
 		}
-	}
+		if (tour.ticketChecked === true) {
+			return;
+		}
+		await sql`CALL cancel_request(${requestId}, ${userId}, ${Date.now()})`.execute(trx);
+		for (const companyOwner of tour.companyOwners) {
+			try {
+				await sendMail(CancelNotificationCompany, 'Stornierte Buchung', companyOwner.email, {
+					events: tour.events,
+					name: companyOwner.name,
+					departure: tour.departure
+				});
+			} catch {
+				console.log(
+					'Failed to send cancellation email to company with email: ',
+					companyOwner.email,
+					' tourId: ',
+					tour.id
+				);
+			}
+		}
+	});
 };
--- src/routes/api/cancelTour/+server.ts
+++ src/routes/api/cancelTour/+server.ts
@@ -12,65 +12,68 @@ export const POST = async (event: RequestEvent) => {
 	if (!company || !p.tourId || p.message == null || p.message == undefined) {
 		return json({});
 	}
-	const tour = await db
-		.selectFrom('tour')
-		.where('tour.id', '=', p.tourId)
-		.select((eb) => [
-			'tour.fare',
-			jsonArrayFrom(
-				eb
-					.selectFrom('request')
-					.innerJoin('user', 'user.id', 'request.customer')
-					.whereRef('tour.id', '=', 'request.tour')
-					.select((eb) => [
-						'user.email',
-						'user.name',
-						'request.ticketChecked',
-						jsonArrayFrom(
-							eb
-								.selectFrom('event')
-								.whereRef('event.request', '=', 'request.id')
-								.orderBy('isPickup', 'asc')
-								.select(['event.address', 'event.communicatedTime'])
-						).as('events')
-					])
-			).as('requests')
-		])
-		.executeTakeFirst();
-	if (tour === undefined) {
-		return json({});
-	}
-	if (tour.requests.some((r) => r.ticketChecked)) {
-		error(400, {
-			message: 'Es wurde bereits ein Ticket gescannt - die Tour kann nicht storniert werden.'
-		});
-	}
-	if (tour.fare !== null) {
-		error(400, {
-			message:
-				'Der Taxameterstand wurde bereits eingetragen - die Tour kann nicht storniert werden.'
-		});
-	}
-	await sql`CALL cancel_tour(${p.tourId}, ${company}, ${p.message})`.execute(db);
-	console.assert(tour.requests.length != 0, 'Found a tour with no requests');
-	for (const request of tour.requests) {
-		console.assert(request.events.length != 0, 'Found a request with no events');
-		try {
-			await sendMail(CancelNotificationCustomer, 'Stornierte Buchung', request.email, {
-				start: request.events[0].address,
-				target: request.events[1].address,
-				startTime: request.events[0].communicatedTime,
-				name: request.name
-			});
-		} catch {
-			console.log(
-				'Failed to send cancellation email to customer with email: ',
-				request.email,
-				' tourId: ',
-				p.tourId
-			);
+	await db.transaction().execute(async (trx) => {
+		await sql`LOCK TABLE tour, request, event, "user" IN ACCESS EXCLUSIVE MODE;`.execute(trx);
+		const tour = await trx
+			.selectFrom('tour')
+			.where('tour.id', '=', p.tourId)
+			.select((eb) => [
+				'tour.fare',
+				jsonArrayFrom(
+					eb
+						.selectFrom('request')
+						.innerJoin('user', 'user.id', 'request.customer')
+						.whereRef('tour.id', '=', 'request.tour')
+						.select((eb) => [
+							'user.email',
+							'user.name',
+							'request.ticketChecked',
+							jsonArrayFrom(
+								eb
+									.selectFrom('event')
+									.whereRef('event.request', '=', 'request.id')
+									.orderBy('isPickup', 'asc')
+									.select(['event.address', 'event.communicatedTime'])
+							).as('events')
+						])
+				).as('requests')
+			])
+			.executeTakeFirst();
+		if (tour === undefined) {
 			return json({});
 		}
-	}
+		if (tour.requests.some((r) => r.ticketChecked)) {
+			error(400, {
+				message: 'Es wurde bereits ein Ticket gescannt - die Tour kann nicht storniert werden.'
+			});
+		}
+		if (tour.fare !== null) {
+			error(400, {
+				message:
+					'Der Taxameterstand wurde bereits eingetragen - die Tour kann nicht storniert werden.'
+			});
+		}
+		await sql`CALL cancel_tour(${p.tourId}, ${company}, ${p.message})`.execute(trx);
+		console.assert(tour.requests.length != 0, 'Found a tour with no requests');
+		for (const request of tour.requests) {
+			console.assert(request.events.length != 0, 'Found a request with no events');
+			try {
+				await sendMail(CancelNotificationCustomer, 'Stornierte Buchung', request.email, {
+					start: request.events[0].address,
+					target: request.events[1].address,
+					startTime: request.events[0].communicatedTime,
+					name: request.name
+				});
+			} catch {
+				console.log(
+					'Failed to send cancellation email to customer with email: ',
+					request.email,
+					' tourId: ',
+					p.tourId
+				);
+				return json({});
+			}
+		}
+	});
 	return json({});
 };
