f56382d931a937041b26c132b95e338566899166
nils penzel <penzel.nils@web.de>
2025-03-24 18:42:18 +0100

    wip

--- e2e/moveTour.test.ts
+++ e2e/moveTour.test.ts
@@ -12,6 +12,7 @@ test('Move tour to vehicle with cancelled tour', async ({ page }) => {
 	await page.mouse.down();
 	await moveMouse(page, `GR-TU-12-${dayString}T08:00:00.000Z`);
 	await page.mouse.up();
+	await page.waitForTimeout(250);
 
 	await expect(page.getByTestId(`GR-TU-11-${dayString}T08:00:00.000Z`).locator('div')).toHaveCSS(
 		'background-color',
@@ -24,7 +25,6 @@ test('Move tour to vehicle with cancelled tour', async ({ page }) => {
 	// Open TourDialog
 	await page.mouse.down();
 	await page.mouse.up();
-	await page.waitForTimeout(250);
 	// Cancel Tour
 	await page.getByText('Stornieren').click();
 	await page.getByRole('textbox').fill('test');
