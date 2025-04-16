f318417c9f8776d0517f60b6d9aaca3a7cba6ef5
nils penzel <penzel.nils@web.de>
2025-04-16 13:12:17 +0200

    wip

--- migrations/2025-03-21-favourites.js
+++ migrations/2025-03-21-favourites.js
@@ -17,8 +17,8 @@ export async function up(db) {
         .createTable('favourite_routes')
         .addColumn('id', 'serial', (col) => col.primaryKey())
         .addColumn('user', 'integer', (col) => col.references('user.id').notNull())
-		.addColumn('from_id', 'integer', (col) => col.references('favourite_locations.id').notNull())
-		.addColumn('to_id', 'integer', (col) => col.references('favourite_locations.id').notNull())
+        .addColumn('from_id', 'integer', (col) => col.references('favourite_locations.id').notNull())
+        .addColumn('to_id', 'integer', (col) => col.references('favourite_locations.id').notNull())
         .addColumn('last_timestamp', 'bigint', (col) => col.notNull())
         .addColumn('count', 'integer', (col) => col.notNull().defaultTo(1))
         .execute();
