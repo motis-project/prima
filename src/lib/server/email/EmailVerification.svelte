d0210bd840a17929674dc967281551c1089a7987
nils penzel <penzel.nils@web.de>
2025-04-17 10:26:26 +0200

    Display code for easy copying in verification email

--- src/lib/server/email/EmailVerification.svelte
+++ src/lib/server/email/EmailVerification.svelte
@@ -11,7 +11,17 @@
 		<a href="{ORIGIN}/account/verify-email/?code={code}" target="_blank">
 			{ORIGIN}/verify-email/?code={code}
 		</a>
+		<br />oder verwenden sie den Code:<br />
 	</p>
+	<div style="width: 100%;">
+		<div
+			style="border: 1px solid #d1d5db; padding: 12px 24px; display: inline-block; margin-left: 20px;"
+		>
+			<p style="margin: 0; font-family: monospace; font-weight: 300;">
+				{code}
+			</p>
+		</div>
+	</div>
 
 	<EmailFooter />
 </div>
