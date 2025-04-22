adf8649bdd3c8b3cb2c1241f0aeabe54760ae2ff
nils <penzel.nils@web.de>
2025-04-22 12:16:43 +0200

    wip

--- src/lib/server/db/lockTables.ts
+++ src/lib/server/db/lockTables.ts
@@ -2,9 +2,7 @@ import type { Database } from '$lib/server/db';
 import { sql } from 'kysely';
 
 export function lockTablesStatement(tables: Array<keyof Database>) {
-	const sortedTables = [...tables].sort();
-	return sql`LOCK TABLE ${sql.join(
-		sortedTables.map((table) => sql`"${table}"`),
-		sql`, `
-	)} IN ACCESS EXCLUSIVE MODE;`;
+	tables.sort();
+	const tablesString = tables.map((t) => `"${t}"`).join(', ');
+	return sql.raw(`LOCK TABLE ${tablesString} IN ACCESS EXCLUSIVE MODE;`);
 }
