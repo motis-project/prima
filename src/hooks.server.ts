91948ac09c0eab8e911b97964a598973dfc1e8a2
Steffen Heger <steffen.heger@gmail.com>
2025-05-02 15:35:32 +0200

    wip

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -54,10 +54,14 @@ const authHandle: Handle = async ({ event, resolve }) => {
 
 export const handle = authHandle;
 
-admin.initializeApp({
-	credential: admin.credential.cert({
-		projectId: env.FIREBASE_PROJECT_ID,
-		privateKey: env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-		clientEmail: env.FIREBASE_CLIENT_EMAIL
-	})
-});
+try {
+	admin.initializeApp({
+		credential: admin.credential.cert({
+			projectId: env.FIREBASE_PROJECT_ID,
+			privateKey: env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+			clientEmail: env.FIREBASE_CLIENT_EMAIL
+		})
+	});
+} catch (e) {
+	console.log(e);
+}
