c4a36862b904d6d5baba6eb62d9b44a8ae893085
Steffen Heger <steffen.heger@gmail.com>
2025-05-02 16:02:49 +0200

    wip

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -58,7 +58,7 @@ try {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: env.FIREBASE_PROJECT_ID,
-			privateKey: env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+			privateKey: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 			clientEmail: env.FIREBASE_CLIENT_EMAIL
 		})
 	});
