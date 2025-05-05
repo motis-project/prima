05a06cad463257fe2a3558eb2c4c40887a2abd85
nilspenzel <116674699+nilspenzel@users.noreply.github.com>
2025-05-05 15:49:36 +0200

    Open tour dialog in accounting, if url has a tourId and user is compa… (#359)
    * Open tour dialog in accounting, if url has a tourId and user is company owner
    
    * wip
    
    * wip

--- src/routes/admin/accounting/+page.server.ts
+++ src/routes/admin/accounting/+page.server.ts
@@ -1,12 +1,16 @@
 import { getCompanyCosts } from '$lib/server/db/getCompanyCosts';
-import type { PageServerLoad } from './$types.js';
+import type { PageServerLoad, RequestEvent } from './$types.js';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async (event: RequestEvent) => {
 	const { tours, earliestTime, latestTime, costPerDayAndVehicle } = await getCompanyCosts();
+	const url = event.url;
+	const tourParam = url.searchParams.get('tourId');
+	const tourId = tourParam === null || isNaN(parseInt(tourParam)) ? undefined : parseInt(tourParam);
 	return {
 		tours: tours.map(({ interval: _, ...rest }) => rest),
 		earliestTime,
 		latestTime,
-		costPerDayAndVehicle
+		costPerDayAndVehicle,
+		tourId
 	};
 };
