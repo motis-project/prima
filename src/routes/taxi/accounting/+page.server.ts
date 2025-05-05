7ecdb0e9eccbdd54b6be0ee054aac2e4f7f16e92
nils penzel <penzel.nils@web.de>
2025-05-05 15:38:08 +0200

    wip

--- src/routes/taxi/accounting/+page.server.ts
+++ src/routes/taxi/accounting/+page.server.ts
@@ -7,10 +7,8 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
 	const tourId = tourParam === null || isNaN(parseInt(tourParam)) ? undefined : parseInt(tourParam);
 
 	const companyId = event.locals.session!.companyId!;
-	const { tours, earliestTime, latestTime, costPerDayAndVehicle } = await getCompanyCosts(
-		companyId,
-		tourId
-	);
+	const { tours, earliestTime, latestTime, costPerDayAndVehicle } =
+		await getCompanyCosts(companyId);
 	return {
 		tours: tours.map(({ interval: _, ...rest }) => rest),
 		earliestTime,
