1fbb0b9100a3ecdcf2fe0500b139960f4c9fdc56
mority <mority@posteo.net>
2025-06-12 16:34:18 +0200

    service area link to map

--- src/routes/(customer)/routing/+page.svelte
+++ src/routes/(customer)/routing/+page.svelte
@@ -505,14 +505,18 @@
 				/>
 			</div>
 			<div class="mx-auto mt-6 space-y-2 text-sm">
-				<em>{@html t.publicTransitTaxi}</em>
-				<p><strong>{@html t.fare}</strong>:<br />3€ {@html t.perPerson} {@html t.perRide}</p>
-				<p><strong>{@html t.bookingDeadline}</strong>:<br />{@html t.bookingDeadlineContent}</p>
+				<p><strong>{@html t.fare}</strong><br />3€ {@html t.perPerson} {@html t.perRide}</p>
+				<p><strong>{@html t.bookingDeadline}</strong><br />{@html t.bookingDeadlineContent}</p>
 				<p>
-					<strong>{@html t.serviceArea}</strong>:<br />{@html t.regionAround} Bad Muskau, Boxberg/O.L.,
-					Gablenz, Groß Düben, Krauschwitz, Schleife, Trebendorf, Weißkeißel Weißwasser/O.L.<br />
+					<button
+						class="link"
+						onclick={() =>
+							pushState('', { showMap: true, selectedItinerary: page.state.selectedItinerary })}
+						><strong>{@html t.serviceArea}</strong></button
+					><br />{@html t.regionAround} Bad Muskau, Boxberg/O.L., Gablenz, Groß Düben, Krauschwitz, Schleife,
+					Trebendorf, Weißkeißel, Weißwasser/O.L.
 				</p>
-				<p><strong>{@html t.serviceTime}</strong>:<br />{@html t.serviceTimeContent}</p>
+				<p><strong>{@html t.serviceTime}</strong><br />{@html t.serviceTimeContent}</p>
 			</div>
 			<p class="mx-auto mt-6 text-sm">
 				<!-- eslint-disable-next-line svelte/no-at-html-tags -->
