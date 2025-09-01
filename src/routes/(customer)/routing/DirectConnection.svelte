ca3ab9d94c7c34a54f720bf106787c8a551c9612
Traines <git@traines.eu>
2025-09-01 10:48:45 +0000

    linting

--- src/routes/(customer)/routing/DirectConnection.svelte
+++ src/routes/(customer)/routing/DirectConnection.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Button, type ButtonProps } from '$lib/shadcn/button';
 	import { formatDurationSec } from './formatDuration';
-	import { getModeStyle, routeColor } from '$lib/ui/modeStyle';
+	import { getModeStyle } from '$lib/ui/modeStyle';
 	import type { Itinerary } from '$lib/openapi';
 
 	const {
@@ -18,11 +18,8 @@
 	const leg = d.legs.find((leg) => leg.mode !== 'WALK') ?? d.legs[0]!;
 </script>
 
-<Button variant="child" {...restProps}>
-	<div
-		class="flex h-8 items-center text-nowrap rounded-lg px-2 py-1 text-sm font-bold"
-		style={routeColor(leg)}
-	>
+<Button variant="default" {...restProps}>
+	<div class="flex h-8 items-center text-nowrap px-2 py-1 text-sm font-bold">
 		{#each modeStyles as [icon, _color, _textColor], i (i)}
 			<svg class="relative mr-1 h-4 w-4 rounded-full">
 				<use xlink:href={`#${icon}`}></use>
