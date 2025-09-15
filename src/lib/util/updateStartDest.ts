af8e856bcb6d2c3afef7701c04f02449302e242b
Traines <git@traines.eu>
2025-09-15 10:40:20 +0200

    display direct walk (#438)
    * display direct walk
    
    * linting
    
    * linting...

--- src/lib/util/updateStartDest.ts
+++ src/lib/util/updateStartDest.ts
@@ -3,19 +3,21 @@ import type { Location } from '$lib/map/Location';
 import type { SignedPlanResponse } from '$lib/planAndSign';
 
 export const updateStartDest = (from: Location, to: Location) => {
+	const updateItinerary = (it: Itinerary) => {
+		if (it.legs[0].from.name === 'START') {
+			it.legs[0].from.name = from.label!;
+		}
+		if (it.legs[it.legs.length - 1].to.name === 'END') {
+			it.legs[it.legs.length - 1].to.name = to.label!;
+		}
+	};
 	return (r: SignedPlanResponse | undefined) => {
 		if (!r) {
 			return r;
 		}
 
-		r.itineraries.forEach((it: Itinerary) => {
-			if (it.legs[0].from.name === 'START') {
-				it.legs[0].from.name = from.label!;
-			}
-			if (it.legs[it.legs.length - 1].to.name === 'END') {
-				it.legs[it.legs.length - 1].to.name = to.label!;
-			}
-		});
+		r.itineraries.forEach(updateItinerary);
+		r.direct.forEach(updateItinerary);
 
 		return r;
 	};
