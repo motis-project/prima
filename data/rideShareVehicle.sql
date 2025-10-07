9133e98fae64f84ecb85ab245db532250b7b94a0
nils penzel <penzel.nils@web.de>
2025-10-07 11:41:25 +0200

    Add ride share vehicle as sql dump

---
+++ data/rideShareVehicle.sql
@@ -0,0 +1,2 @@
+INSERT INTO "ride_share_vehicle" ("id", "passengers", "luggage", "color", "model", "smoking_allowed", "license_plate", "owner") VALUES
+(1,	3,	0,	'blue',	'Smart',	'0',	'DA-AB-123',	1);
