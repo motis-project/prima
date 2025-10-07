b7d9b20d93abe44ad1c0f19a1af1b49d48df0ad2
nils penzel <penzel.nils@web.de>
2025-10-07 17:12:29 +0200

    Await promises in planAndSign

--- src/routes/api/planAndSign/+server.ts
+++ src/routes/api/planAndSign/+server.ts
@@ -24,53 +24,57 @@ export const POST = async (event: RequestEvent) => {
 	}
 	return json({
 		...response!,
-		itineraries: response!.itineraries.map(async (i) => {
-			const odmLeg1 = i.legs.find(isOdmLeg);
-			const odmLeg2 = i.legs.findLast(isOdmLeg);
-			const rideShareTourFirstLeg = isRideShareLeg(i.legs[0])
-				? parseInt(i.legs[0].tripId!)
-				: undefined;
-			const rideShareTourLastLeg =
-				i.legs.length > 1 && isRideShareLeg(i.legs[i.legs.length - 1])
-					? parseInt(i.legs[i.legs.length - 1].tripId!)
-					: undefined;
-			let rideShareTourInfoFirstLeg: RideShareTourInfo | undefined = undefined;
-			if (rideShareTourFirstLeg !== undefined) {
-				rideShareTourInfoFirstLeg = await getRideShareInfo(rideShareTourFirstLeg);
-			}
-			let rideShareTourInfoLastLeg: RideShareTourInfo | undefined = undefined;
-			if (rideShareTourLastLeg !== undefined) {
-				rideShareTourInfoLastLeg = await getRideShareInfo(rideShareTourLastLeg);
-			}
-			return {
-				...i,
-				signature1:
-					odmLeg1 !== undefined
-						? signEntry(
-								odmLeg1.from.lat,
-								odmLeg1.from.lon,
-								odmLeg1.to.lat,
-								odmLeg1.to.lon,
-								new Date(odmLeg1.startTime).getTime(),
-								new Date(odmLeg1.endTime).getTime(),
-								false
-							)
-						: undefined,
-				signature2:
-					odmLeg2 !== undefined
-						? signEntry(
-								odmLeg2.from.lat,
-								odmLeg2.from.lon,
-								odmLeg2.to.lat,
-								odmLeg2.to.lon,
-								new Date(odmLeg2.startTime).getTime(),
-								new Date(odmLeg2.endTime).getTime(),
-								true
-							)
-						: undefined,
-				rideShareTourInfoFirstLeg,
-				rideShareTourInfoLastLeg
-			};
-		})
+		itineraries: await Promise.all(
+			response!.itineraries.map(async (i) => {
+				const odmLeg1 = i.legs.find(isOdmLeg);
+				const odmLeg2 = i.legs.findLast(isOdmLeg);
+				let rideShareTourInfoFirstLeg: RideShareTourInfo | undefined = undefined;
+				let rideShareTourInfoLastLeg: RideShareTourInfo | undefined = undefined;
+				if (i.legs.length !== 0) {
+					const rideShareTourFirstLeg = isRideShareLeg(i.legs[0])
+						? parseInt(i.legs[0].tripId!)
+						: undefined;
+					const rideShareTourLastLeg =
+						i.legs.length > 1 && isRideShareLeg(i.legs[i.legs.length - 1])
+							? parseInt(i.legs[i.legs.length - 1].tripId!)
+							: undefined;
+					if (rideShareTourFirstLeg !== undefined) {
+						rideShareTourInfoFirstLeg = await getRideShareInfo(rideShareTourFirstLeg);
+					}
+					if (rideShareTourLastLeg !== undefined) {
+						rideShareTourInfoLastLeg = await getRideShareInfo(rideShareTourLastLeg);
+					}
+				}
+				return {
+					...i,
+					signature1:
+						odmLeg1 !== undefined
+							? signEntry(
+									odmLeg1.from.lat,
+									odmLeg1.from.lon,
+									odmLeg1.to.lat,
+									odmLeg1.to.lon,
+									new Date(odmLeg1.startTime).getTime(),
+									new Date(odmLeg1.endTime).getTime(),
+									false
+								)
+							: undefined,
+					signature2:
+						odmLeg2 !== undefined
+							? signEntry(
+									odmLeg2.from.lat,
+									odmLeg2.from.lon,
+									odmLeg2.to.lat,
+									odmLeg2.to.lon,
+									new Date(odmLeg2.startTime).getTime(),
+									new Date(odmLeg2.endTime).getTime(),
+									true
+								)
+							: undefined,
+					rideShareTourInfoFirstLeg,
+					rideShareTourInfoLastLeg
+				};
+			})
+		)
 	});
 };
