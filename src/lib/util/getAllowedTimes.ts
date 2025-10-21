94c8a1aac6836b2e79191f87dae303ec0c97cb8c
nils <penzel.nils@web.de>
2025-10-22 00:07:12 +0200

    wip

--- src/lib/util/getAllowedTimes.ts
+++ src/lib/util/getAllowedTimes.ts
@@ -22,5 +22,6 @@ export function getAllowedTimes(
 		const offset = getOffset(noon.getTime());
 		allowedTimes.push(new Interval(t + startOnDay - offset, t + endOnDay - offset));
 	}
+	console.log("allowed Times:", allowedTimes.map((t) => t.toString()));
 	return allowedTimes;
 }
