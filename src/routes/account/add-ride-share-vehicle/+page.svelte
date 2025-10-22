8f14bf7c1e6bad9ecd99c8d2fbc01a010587af24
nilspenzel <116674699+nilspenzel@users.noreply.github.com>
2025-10-22 13:44:22 +0200

    Change car default picture (#464)
    * Use separate constants for default pictures of profile and car
    
    * Add car default picture
    
    ---------
    
    Co-authored-by: nils_penzel <penzel.nils @web.de>

--- src/routes/account/add-ride-share-vehicle/+page.svelte
+++ src/routes/account/add-ride-share-vehicle/+page.svelte
@@ -10,6 +10,7 @@
 	import Checkbox from '$lib/shadcn/checkbox/checkbox.svelte';
 	import Panel from '$lib/ui/Panel.svelte';
 	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
+	import { defaultCarPicture } from '$lib/constants.js';
 
 	const { form } = $props();
 	let v = $derived(undefined);
@@ -110,7 +111,7 @@
 			name="smokingAllowed"
 			value={smokingAllowed === smokingOptions[0] ? '0' : '1'}
 		/>
-		<UploadPhoto name="vehiclePicture" />
+		<UploadPhoto name="vehiclePicture" defaultPicture={defaultCarPicture} />
 		<Button type="submit" variant="outline" data-testid="create-vehicle">
 			{v == undefined ? t.rideShare.createVehicle : t.rideShare.saveChanges}
 		</Button>
