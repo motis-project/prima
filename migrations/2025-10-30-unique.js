6737bc5d4602caf0148accc23d043f01b9970641
nils penzel <penzel.nils@web.de>
2025-11-05 08:54:55 +0100

    Make ride share license plates unique per user

--- migrations/2025-10-30-unique.js
+++ migrations/2025-10-30-unique.js
@@ -1,7 +1,7 @@
 export async function up(db) {
 	await db.schema
 		.alterTable('ride_share_vehicle')
-		.addUniqueConstraint('ride_share_vehicle_license_plate', ['license_plate'])
+		.addUniqueConstraint('ride_share_vehicle_license_plate', ['license_plate', 'owner'])
 		.execute();
 }
 
