47d73d5ca5ba78cd45f6366d021205c5149ee9e5
mority <mority@posteo.net>
2025-11-18 17:26:27 +0100

    format

--- src/routes/(customer)/routing/PoolingBadge.svelte
+++ src/routes/(customer)/routing/PoolingBadge.svelte
@@ -6,7 +6,6 @@
 <div
 	class="flex h-7 w-fit items-center text-nowrap rounded-lg border-2 border-solid bg-black px-1 py-1 text-sm font-bold"
 >
-	<Users class="relative h-4 w-4 rounded-full" /><Leaf
-		class="relative h-4 w-4 rounded-full text-green-500"
-	/>
+	<Users class="relative h-4 w-4 rounded-full" />
+	<Leaf class="relative h-4 w-4 rounded-full text-green-500" />
 </div>
