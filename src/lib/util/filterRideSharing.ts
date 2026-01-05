0369ef7b9f0d045ae3b7f6e2af8ed7a4a6e1ac56
mority <mority@posteo.net>
2026-01-05 18:11:44 +0100

    filter ride sharing

---
+++ src/lib/util/filterRideSharing.ts
@@ -0,0 +1,65 @@
+import { type Itinerary } from '$lib/openapi';
+import { isRideShareLeg, isOdmLeg } from '$lib/util/booking/checkLegType';
+import { dominanceFilter } from '$lib/util/dominanceFilter';
+
+export function filterRideSharing(itineraries: Array<Itinerary>): Array<Itinerary> {
+	return dominanceFilter(dominanceFilter(itineraries, publicTransitDominates), rideShareDominates);
+}
+
+function publicTransitDominates(a: Itinerary, b: Itinerary): boolean {
+	return publicTransitOnly(a) && usesRideSharing(b) && paretoDominates(a, b);
+}
+
+function rideShareDominates(a: Itinerary, b: Itinerary): boolean {
+	return usesSameTour(a, b) && paretoDominates(a, b);
+}
+
+function publicTransitOnly(i: Itinerary): boolean {
+	for (let l of i.legs) {
+		if (isOdmLeg(l)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function usesRideSharing(i: Itinerary): boolean {
+	for (let l of i.legs) {
+		if (isRideShareLeg(l)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function usesSameTour(a: Itinerary, b: Itinerary): boolean {
+	for (let x of a.legs) {
+		if (isRideShareLeg(x) && 'tripId' in x && typeof x.tripId === 'string') {
+			let xProperties = JSON.parse(x.tripId);
+			if (!('tour' in xProperties) || typeof xProperties.tour !== 'number') {
+				continue;
+			}
+			for (let y of b.legs) {
+				if (isRideShareLeg(y) && 'tripId' in y && typeof y.tripId === 'string') {
+					let yProperties = JSON.parse(y.tripId);
+					if (!('tour' in yProperties) || typeof yProperties.tour !== 'number') {
+						continue;
+					}
+					if (xProperties.tour === yProperties.tour) {
+						return true;
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function paretoDominates(a: Itinerary, b: Itinerary): boolean {
+	return (
+		a.startTime >= b.startTime &&
+		a.endTime <= b.endTime &&
+		a.transfers <= b.transfers &&
+		(a.startTime > b.startTime || a.endTime < b.endTime || a.transfers < b.transfers)
+	);
+}
