9e1f0545f5d36132c9497a71c7000f55981925f1
mority <mority@posteo.net>
2026-01-06 18:55:32 +0100

    wip

--- src/lib/util/mixer.ts
+++ src/lib/util/mixer.ts
@@ -1,6 +1,6 @@
 import { type Itinerary } from '$lib/openapi';
 import { isTaxiLeg } from './booking/checkLegType';
-import { isDirectTaxi } from './itineraryHelpers';
+import { isDirectTaxi, publicTransitOnly, usesTaxi } from './itineraryHelpers';
 
 export function mix(
 	itineraries: Array<Itinerary>,
@@ -15,7 +15,7 @@ export function mix(
 		return [itineraries, [], []];
 	}
 
-	const cost = (i: Itinerary): number => {
+	const getCost = (i: Itinerary): number => {
 		return (
 			i.legs
 				.map((l) =>
@@ -29,6 +29,38 @@ export function mix(
 		);
 	};
 
-	const start = new Date(itineraries[0].startTime);
-	const end = new Date(itineraries[itineraries.length - 1].endTime);
+	const start = Math.floor(new Date(itineraries[0].startTime).getTime() / 60000);
+	const end = Math.ceil(new Date(itineraries[itineraries.length - 1].endTime).getTime() / 60000);
+
+	const getIndex = (t: Date) => {
+		return Math.round(t.getTime() / 60000) - start;
+	};
+
+	const getCenter = (i: Itinerary): Date => {
+		return new Date(
+			Math.round((new Date(i.startTime).getTime() + (i.duration * 1000) / 2) / 60000) * 60000
+		);
+	};
+
+	const getThreshold = (is: Array<Itinerary>, slope: number): Array<number> => {
+		let threshold = new Array<number>(end - start);
+		threshold.fill(Number.POSITIVE_INFINITY);
+
+		for (const i of is) {
+			const cost = getCost(i);
+			const center = getCenter(i);
+			const localThreshold = (t: Date): number => {
+				return slope * Math.abs(Math.round((center.getTime() - t.getTime()) / 60000)) + cost;
+			};
+		}
+
+		return threshold;
+	};
+
+	const ptItineraries = itineraries.filter((i) => publicTransitOnly(i));
+	const ptThreshold = getThreshold(ptItineraries, ptSlope);
+	const taxiItineraries = itineraries.filter((i) => usesTaxi(i));
+	const taxiThreshold = getThreshold(taxiItineraries, taxiSlope);
+
+	return [itineraries, ptThreshold, taxiThreshold];
 }
