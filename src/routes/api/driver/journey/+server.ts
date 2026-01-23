0e73cea24a76f1b0b7b978de79d7b9660dc41602
Steffen Heger <steffen.heger@gmail.com>
2026-01-23 17:25:32 +0100

    format

--- src/routes/api/driver/journey/+server.ts
+++ src/routes/api/driver/journey/+server.ts
@@ -4,25 +4,25 @@ import { json } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
 
 export const GET = async ({ url }) => {
-    const requestId = readInt(url.searchParams.get('requestId'));
+	const requestId = readInt(url.searchParams.get('requestId'));
 
-    if (isNaN(requestId)) {
-        error(400, { message: 'Invalid requestId parameter' });
-    }
+	if (isNaN(requestId)) {
+		error(400, { message: 'Invalid requestId parameter' });
+	}
 
-    const itinerary = await db
-        .selectFrom('journey')
-        .where('request1', '=', requestId)
-        .select('journey.json')
-        .executeTakeFirst();
+	const itinerary = await db
+		.selectFrom('journey')
+		.where('request1', '=', requestId)
+		.select('journey.json')
+		.executeTakeFirst();
 
-    if (itinerary != undefined) {
-        itinerary.json.legs.forEach(e => {
-            e.steps = undefined
-            e.legGeometry.points = ""
-        })
-        return json(itinerary.json);
-    } else {
-        return new Response(null, { status: 404 });
-    }
+	if (itinerary != undefined) {
+		itinerary.json.legs.forEach((e) => {
+			e.steps = undefined;
+			e.legGeometry.points = '';
+		});
+		return json(itinerary.json);
+	} else {
+		return new Response(null, { status: 404 });
+	}
 };
