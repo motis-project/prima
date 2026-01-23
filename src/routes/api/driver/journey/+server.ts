95fc7344655445a9af8a1bf726f34989e53fe9c9
Steffen Heger <71780550+steffenheger@users.noreply.github.com>
2026-01-23 17:34:38 +0100

    Driver journey api (#571)
    * add journey api for driver app
    
    * format

---
+++ src/routes/api/driver/journey/+server.ts
@@ -0,0 +1,28 @@
+import { db } from '$lib/server/db';
+import { readInt } from '$lib/server/util/readForm.js';
+import { json } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
+
+export const GET = async ({ url }) => {
+	const requestId = readInt(url.searchParams.get('requestId'));
+
+	if (isNaN(requestId)) {
+		error(400, { message: 'Invalid requestId parameter' });
+	}
+
+	const itinerary = await db
+		.selectFrom('journey')
+		.where('request1', '=', requestId)
+		.select('journey.json')
+		.executeTakeFirst();
+
+	if (itinerary != undefined) {
+		itinerary.json.legs.forEach((e) => {
+			e.steps = undefined;
+			e.legGeometry.points = '';
+		});
+		return json(itinerary.json);
+	} else {
+		return new Response(null, { status: 404 });
+	}
+};
