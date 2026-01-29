86629c020c5d839acff0a421a0b8ecf8098c9713
Traines <git@traines.eu>
2026-01-29 16:56:58 +0000

    linting

--- src/routes/account/login/+server.ts
+++ src/routes/account/login/+server.ts
@@ -6,6 +6,6 @@ export const POST = async (event: RequestEvent) => {
 	await login(event, LoginInto.PRIMA_DRIVER);
 };
 
-export const GET = async (event: RequestEvent) => {
+export const GET = async () => {
 	return redirect(303, '/account/ui-login');
 };
