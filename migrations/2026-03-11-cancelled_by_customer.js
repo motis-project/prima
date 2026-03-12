70a7a01753e8de6518ed63e2fb80db5470cf609b
nils penzel <penzel.nils@web.de>
2026-03-12 14:08:33 +0100

    Store info when a tour/request/rideShareTour is canceled

--- migrations/2026-03-11-cancelled_by_customer.js
+++ migrations/2026-03-11-cancelled_by_customer.js
@@ -4,6 +4,7 @@ export async function up(db) {
     await db.schema
         .alterTable('request')
         .addColumn('cancelled_by_customer', 'boolean', (col) => col.notNull().defaultTo(false))
+        .addColumn('cancelled_at', 'bigint')
         .execute();
 
     await sql`
@@ -38,7 +39,8 @@ export async function up(db) {
     
             UPDATE request r
             SET cancelled = true,
-                cancelled_by_customer = true
+                cancelled_by_customer = true,
+                cancelled_at = p_now
             WHERE r.id = p_request_id;
     
             SELECT tour INTO v_tour_id
@@ -67,7 +69,8 @@ export async function up(db) {
     await sql`
     CREATE OR REPLACE PROCEDURE cancel_ride_share_request(
     	p_request_id INTEGER,
-    	p_user_id INTEGER
+    	p_user_id INTEGER,
+        p_now BIGINT
     ) AS  $$
     DECLARE
     	v_tour_id INTEGER;
@@ -83,7 +86,8 @@ export async function up(db) {
 
     	UPDATE request r
     	SET cancelled = true,
-            cancelled_by_customer = true
+            cancelled_by_customer = true,
+            cancelled_at = p_now
     	WHERE r.id = p_request_id;
 
     	UPDATE event e
@@ -91,6 +95,84 @@ export async function up(db) {
     	WHERE e.request = p_request_id;
     END;
     $$ LANGUAGE plpgsql;
+    `.execute(db);
+
+    await db.schema
+        .alterTable('tour')
+        .addColumn('cancelled_at', 'bigint')
+        .execute();
+
+    await sql`
+    CREATE OR REPLACE PROCEDURE cancel_tour(
+    	p_tour_id INTEGER,
+    	p_company_id INTEGER,
+    	p_message VARCHAR,
+        p_now BIGINT
+    ) AS $$
+    BEGIN
+    	IF NOT EXISTS (
+    	    SELECT 1
+    	    FROM tour t
+    	    JOIN vehicle v ON v.id = t.vehicle
+    	    WHERE t.id = p_tour_id
+    	    AND v.company = p_company_id
+    	) THEN
+    	    RETURN;
+    	END IF;
+
+    	UPDATE tour t
+    	SET cancelled = TRUE,
+    		message = p_message,
+            cancelled_at = p_now
+    	WHERE t.id = p_tour_id;
+
+    	UPDATE request r
+    	SET cancelled = TRUE
+    	WHERE r.tour = p_tour_id;
+
+    	UPDATE event e
+    	SET cancelled = TRUE
+    	WHERE e.request IN (SELECT id FROM request WHERE tour = p_tour_id);
+    END;
+    $$ LANGUAGE plpgsql;
+    `.execute(db);
+
+    await db.schema
+        .alterTable('ride_share_tour')
+        .addColumn('cancelled_at', 'bigint')
+        .execute();
+
+    await sql`
+        CREATE OR REPLACE PROCEDURE cancel_ride_share_tour(
+        	p_tour_id INTEGER,
+        	p_user_id INTEGER,
+            p_now BIGINT
+        ) AS $$
+        BEGIN
+        	IF NOT EXISTS (
+        	    SELECT 1
+        	    FROM ride_share_tour t
+        	    JOIN ride_share_vehicle v ON v.id = t.vehicle
+        	    WHERE t.id = p_tour_id
+        	    AND v.owner = p_user_id
+        	) THEN
+        	    RETURN;
+        	END IF;
+
+        	UPDATE ride_share_tour t
+        	SET cancelled = TRUE,
+            cancelled_at = p_now
+        	WHERE t.id = p_tour_id;
+
+        	UPDATE request r
+        	SET cancelled = TRUE
+        	WHERE r.ride_share_tour = p_tour_id;
+
+        	UPDATE event e
+        	SET cancelled = TRUE
+        	WHERE e.request IN (SELECT id FROM request WHERE ride_share_tour = p_tour_id);
+        END;
+        $$ LANGUAGE plpgsql;
     `.execute(db);
 }
 
