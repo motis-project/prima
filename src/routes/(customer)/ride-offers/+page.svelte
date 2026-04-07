8816e112ff53c840dd89fb45a38be7bba09104c7
nils <penzel.nils@web.de>
2026-04-07 11:10:23 +0200

    Fix sorting of ride share notification list

--- src/routes/(customer)/ride-offers/+page.svelte
+++ src/routes/(customer)/ride-offers/+page.svelte
@@ -8,6 +8,7 @@
 	import RoutingNotifications from '$lib/ui/RoutingNotifications.svelte';
 
 	const { data } = $props();
+	let notificationRows = $state(data.notifications);
 	const pastJourneys = data.journeys.filter(
 		(j) => j.cancelled || new Date(j.journey.endTime).getTime() < Date.now()
 	);
@@ -61,7 +62,7 @@
 		{t.noRideOffers}
 	{/if}
 	{#if data.notifications.length !== 0}
-		<RoutingNotifications rows={data.notifications}></RoutingNotifications>
+		<RoutingNotifications bind:rows={notificationRows}></RoutingNotifications>
 	{/if}
 	{@render journeyList(plannedJourneys)}
 	{#if pastJourneys.length !== 0}
