0ca2b90e522a4fd5f66a60f7df189d7ddebf5541
nils <penzel.nils@web.de>
2026-04-20 21:14:52 +0200

    Add test running simulation for taxis

---
+++ src/lib/server/simulation/simulation.test.ts
@@ -0,0 +1,29 @@
+import { beforeAll, describe, it } from 'vitest';
+import { simulation, type SimulationParams } from './simulation';
+import { MINUTE } from '$lib/util/time';
+import { clearDatabase } from '$lib/testHelpers';
+
+beforeAll(async () => {
+	await clearDatabase();
+});
+
+const duration = 3 * MINUTE;
+
+describe(
+	'simulation as test',
+	() => {
+		it('simulation', async () => {
+			const p: SimulationParams = {
+				restrict: true,
+				finishTime: Date.now() + duration,
+				mode: 'taxi',
+				healthChecks: true,
+				full: true,
+				companies: 3,
+				vehiclesPerCompany: 3
+			};
+			await simulation(p);
+		});
+	},
+	MINUTE + duration
+);
