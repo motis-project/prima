6dc95524be02af184c7fa601c9e4ba2b2efd73b1
mority <mority@posteo.net>
2026-05-19 01:17:23 +0200

    cleanup

--- src/lib/circle.ts
+++ src/lib/circle.ts
@@ -6,7 +6,7 @@ const steps = 72;
 export function circlePolygon(center: Position, radius: number): Position[][] {
 	const polygon = new Array<Position>();
 	for (let i = 0; i != steps; ++i) {
-		polygon.push(haversineDestination(center, radius, ((i * 1) / steps) * 360));
+		polygon.push(haversineDestination(center, radius, (i / steps) * 360));
 	}
 	polygon.push(polygon[0]);
 	return [polygon];
