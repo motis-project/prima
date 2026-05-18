959eff00b301330b7452bf71560818823f052a58
mority <mority@posteo.net>
2026-05-19 01:09:54 +0200

    lint

--- src/lib/circle.ts
+++ src/lib/circle.ts
@@ -4,7 +4,7 @@ import { haversineDestination } from './haversine';
 const steps = 72;
 
 export function circlePolygon(center: Position, radius: number): Position[][] {
-	let polygon = new Array<Position>();
+	const polygon = new Array<Position>();
 	for (let i = 0; i != steps; ++i) {
 		polygon.push(haversineDestination(center, radius, ((i * 1) / steps) * 360));
 	}
