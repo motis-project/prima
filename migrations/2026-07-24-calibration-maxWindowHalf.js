175a764d50f77edd91662f6415a548acc6b66cab
Traines <git@traines.eu>
2026-07-26 15:13:27 +0000

    fix migration

--- migrations/2026-07-24-calibration-maxWindowHalf.js
+++ migrations/2026-07-24-calibration-maxWindowHalf.js
@@ -1,7 +1,7 @@
 export async function up(db) {
     await db.schema
 		.alterTable('taxi_filter')
-		.addColumn('damping_window', 'real', (col) => col.notNull())
+		.addColumn('damping_window', 'real', (col) => col.notNull().defaultTo(0))
 		.execute();
 }
 
